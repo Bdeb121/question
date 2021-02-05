be78217d0cda8cf09080f427b18a9f731c7bd2a7
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-02-05 19:42:54 +0530

    Update question.js

--- question.js
+++ question.js
@@ -1,1 +1,65 @@
+var quiz = [
+  {
+    question: 'What is the largest organ of the human body?',
+    options: ['Brain', 'Skeleton', 'Heart', 'Skin'],
+    answer: 4,
+  },
+  {
+    question: 'What is the term for cell death?',
+    options: ['Apoptosis', 'Mitosis', 'Osmosis', 'Diffusion'],
+    answer: 1,
+  },
+  {
+    question: 'What does the acronym DNA stand for?',
+    options: [
+      'Dinitrogen Acetate',
+      'Determined Not Active',
+      'Deoxyribonucleic acid',
+      'Denucleated actins',
+    ],
+    answer: 3,
+  },
+  {
+    question:
+      'Name the process used by plants to convert light energy into chemical energy.',
+    options: ['Photosynthesis', 'Mitosis', 'Absorption', 'Diffusion'],
+    answer: 1,
+  },
+  {
+    question: 'What is a fertilized egg called?',
+    options: ['Larva', 'Zygote', 'Embryo', 'Fetus'],
+    answer: 2,
+  },
+  {
+    question: 'Which hormone is associated with the fight-or-flight response?',
+    options: [
+      'Dopamine',
+      'Testosterone',
+      'Thyroid Stimulating Hormone',
+      'Norepinephrine',
+    ],
+    answer: 4,
+  },
+  {
+    question:
+      "Name the eukaryotic cell organelle that contains the majority of the cell's genetic material.",
+    options: ['Mitochondria', 'Nucleus', 'Riboflavin', 'Golgi body'],
+    answer: 2,
+  },
+  {
+    question: 'What is the longest bone in the human body?',
+    options: ['Femur', 'Tibia', 'Fibia', 'Humerus'],
+    answer: 1,
+  },
+  {
+    question: 'Which molecule is used to store energy for cells?',
+    options: ['DNA', 'ATP', 'RNA', 'cAMP'],
+    answer: 2,
+  },
+  {
+    question: 'How many chromosomes does a person have?',
+    options: ['23', '46', '92', '138'],
+    answer: 2,
+  },
+];
 
