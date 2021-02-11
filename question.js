d58e210040a06e8f5b3c67440e9e1f2647ffaed3
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-02-11 21:00:09 +0530

    Update question.js

--- question.js
+++ question.js
@@ -1,96 +1,34 @@
-var quiz = [
-  {
-    question: 'What is the largest organ of the human body?',
-    options: ['Brain', 'Skeleton', 'Heart', 'Skin'],
-    answer: 4,
-  },
-  {
-    question: 'What is the term for cell death?',
-    options: ['Apoptosis', 'Mitosis', 'Osmosis', 'Diffusion'],
-    answer: 1,
-  },
-  {
-    question: 'What does the acronym DNA stand for?',
-    options: [
-      'Dinitrogen Acetate',
-      'Determined Not Active',
-      'Deoxyribonucleic acid',
-      'Denucleated actins',
-    ],
-    answer: 3,
-  },
-  {
-    question:
-      'Name the process used by plants to convert light energy into chemical energy.',
-    options: ['Photosynthesis', 'Mitosis', 'Absorption', 'Diffusion'],
-    answer: 1,
-  },
-  {
-    question: 'What is a fertilized egg called?',
-    options: ['Larva', 'Zygote', 'Embryo', 'Fetus'],
-    answer: 2,
-  },
-  {
-    question: 'Which hormone is associated with the fight-or-flight response?',
-    options: [
-      'Dopamine',
-      'Testosterone',
-      'Thyroid Stimulating Hormone',
-      'Norepinephrine',
-    ],
-    answer: 4,
-  },
-  {
-    question:
-      "Name the eukaryotic cell organelle that contains the majority of the cell's genetic material.",
-    options: ['Mitochondria', 'Nucleus', 'Riboflavin', 'Golgi body'],
-    answer: 2,
-  },
-  {
-    question: 'What is the longest bone in the human body?',
-    options: ['Femur', 'Tibia', 'Fibia', 'Humerus'],
-    answer: 1,
-  },
-  {
-    question: 'Which molecule is used to store energy for cells?',
-    options: ['DNA', 'ATP', 'RNA', 'cAMP'],
-    answer: 2,
-  },
-  {
-    question: 'How many chromosomes does a person have?',
-    options: ['23', '46', '92', '138'],
-    answer: 2,
-  },
-  {
+let quiz = [
+    {
         question:
             'If the length of E.coli DNA is 1.36 mm, can you calculate the number of base pairs in E.coli?',
         options: [
-            'a) 1360000 bp',
-            'b) 4000000 bp',
-            'c) 34000 bp',
-            'd) 272000 bp',
+            '1360000 bp',
+            '4000000 bp',
+            '34000 bp',
+            '272000 bp',
         ],
         answer: 2,
     },
     {
         question:
             'If an E.coli DNA is having 1600 bp length, it may have how many nucleosome?',
-        options: ['a) Six  ', 'b) Ten ', 'c) Thirty four  ', 'd) None '],
+        options: ['Six  ', 'Ten ', 'Thirty four  ', 'None '],
         answer: 4,
     },
     {
         question:
             'How many nucleotides are present in Bacteriophage Φ (phi)- 174?',
-        options: ['a) 48502 BP  ', 'b) 2130 ', 'c) 5134 BP  ', 'd) 5386 '],
+        options: ['48502 BP  ', '2130 ', '5134 BP  ', '5386 '],
         answer: 4,
     },
     {
         question: "Chargaff's rule not applicable on?",
         options: [
-            'a) Lambda Phage DNA  ',
-            'b) DNA of E.Coli ',
-            'c) Plasmid of Bacteria  ',
-            'd) Genome of bacteriophage Φ (phi)- 174 ',
+            'Lambda Phage DNA  ',
+            'DNA of E.Coli ',
+            'Plasmid of Bacteria  ',
+            'Genome of bacteriophage Φ (phi)- 174 ',
         ],
         answer: 4,
     },
@@ -98,94 +36,94 @@ var quiz = [
         question:
             'The amount of purines is equal to the amount of pyrimidines was suggested by?',
         options: [
-            'a) Robert Brown ',
-            'b) Meischer',
-            'c) Chargaff ',
-            'd) Khorana',
+            'Robert Brown ',
+            'Meischer',
+            'Chargaff ',
+            'Khorana',
         ],
         answer: 3,
     },
     {
         question:
             'The number of pairs of hydrogen bonds in the DNA molecule of 150 base pairs having 60 AT pairs would be?',
-        options: ['a) 300  ', 'b) 150 ', 'c) 390  ', 'd) 490 '],
+        options: ['300  ', '150 ', '390  ', '490 '],
         answer: 3,
     },
     {
         question: 'Watson and Crick proposed the model of DNA in?',
-        options: ['a) 1955  ', 'b) 1953 ', 'c) 1943  ', 'd) 1963 '],
+        options: ['1955  ', '1953 ', '1943  ', '1963 '],
         answer: 2,
     },
     {
         question:
             'The linkage of nucleosome units discovered by Olins and Olins is by?',
         options: [
-            'a) Histone H3  ',
-            'b) Histone H4 ',
-            'c) Linker DNA  ',
-            'd) RNA ',
+            'Histone H3  ',
+            'Histone H4 ',
+            'Linker DNA  ',
+            'RNA ',
         ],
         answer: 3,
     },
     {
         question: 'Thymine is also called?',
         options: [
-            'a) 2 methyl uracil  ',
-            'b) 3 methyl uracil ',
-            'c) 4 mithyl uracil  ',
-            'd) 5 methyl uracil ',
+            '2 methyl uracil  ',
+            '3 methyl uracil ',
+            '4 mithyl uracil  ',
+            '5 methyl uracil ',
         ],
         answer: 4,
     },
     {
         question: 'X-ray diffraction data of DNA was produced by?',
         options: [
-            'a) Watson and Crick ',
-            'b) WilKins and Franklin',
-            'c) Betason and Punnett ',
-            'd) Both (a) and (b)',
+            'Watson and Crick ',
+            'WilKins and Franklin',
+            'Betason and Punnett ',
+            'Both a) and b)',
         ],
         answer: 2,
     },
     {
         question: 'Nucleotide arrangment in DNA can be seen by?',
         options: [
-            'a) X-ray crystallography  ',
-            'b) Electron microscope ',
-            'c) Ultracentrifuge  ',
-            'd) Light Microscope ',
+            'X-ray crystallography  ',
+            'Electron microscope ',
+            'Ultracentrifuge  ',
+            'Light Microscope ',
         ],
         answer: 1,
     },
     {
         question: 'The phosphate group in nucleic acids provided by?',
-        options: ['a) ATP  ', 'b) P2O5 ', 'c) H3PO4  ', 'd) H2PO4 '],
+        options: ['ATP  ', 'P2O5 ', 'H3PO4  ', 'H2PO4 '],
         answer: 3,
     },
     {
         question:
             'The concept of central dogma in molecular biology was given by?',
         options: [
-            'a) Francis Crick  ',
-            'b) James Watson ',
-            'c) Maurice Wilkins  ',
-            'd) Wilkins and Franklin ',
+            'Francis Crick  ',
+            'James Watson ',
+            'Maurice Wilkins  ',
+            'Wilkins and Franklin ',
         ],
         answer: 1,
     },
     {
         question:
             'The flow of Genetic information or central dogma is observed to be bidirectional in?',
-        options: ['a) Bacteria  ', 'b) Virus ', 'c) Protista  ', 'd) Fungi '],
+        options: ['Bacteria  ', 'Virus ', 'Protista  ', 'Fungi '],
         answer: 2,
     },
     {
         question: 'Intiator amino acid in the polypeptide chain is?',
         options: [
-            'a) Methionine  ',
-            'b) Tyrosine ',
-            'c) Histidine  ',
-            'd) Arginine ',
+            'Methionine  ',
+            'Tyrosine ',
+            'Histidine  ',
+            'Arginine ',
         ],
         answer: 1,
     },
@@ -193,76 +131,76 @@ var quiz = [
         question:
             'The ion required to keep the two ribosomal units together during protein synthesis is?',
         options: [
-            'a) Potassium ion  ',
-            'b) Magnesium ion ',
-            'c) Chloride ion  ',
-            'd) All the above ',
+            'Potassium ion  ',
+            'Magnesium ion ',
+            'Chloride ion  ',
+            'All the above ',
         ],
         answer: 2,
     },
     {
         question: 'In Ribosome that acts as a catalyst or ribozyme is?',
         options: [
-            'a) 40 S rRNA  ',
-            'b) 50 S rRNA ',
-            'c) 70 S rRNA  ',
-            'd) 23 S rRNA ',
+            '40 S rRNA  ',
+            '50 S rRNA ',
+            '70 S rRNA  ',
+            '23 S rRNA ',
         ],
         answer: 4,
     },
     {
         question: 'Protein are formed by?',
         options: [
-            'a) Peptide link  ',
-            'b) Glycosidic link ',
-            'c) Phosphate link  ',
-            'd) Phosphodiamine linkage ',
+            'Peptide link  ',
+            'Glycosidic link ',
+            'Phosphate link  ',
+            'Phosphodiamine linkage ',
         ],
         answer: 1,
     },
     {
         question: ' The replication of nuclear DNA occurs in?',
         options: [
-            'a) G1phase  ',
-            'b) G2 phase ',
-            'c) S phase  ',
-            'd) M phase ',
+            'G1phase  ',
+            'G2 phase ',
+            'S phase  ',
+            'M phase ',
         ],
         answer: 3,
     },
     {
         question: 'In a polynucleotide chain?',
         options: [
-            "a) Free phosphate moiety at 5'-end  ",
-            "b) Free phosphate moiety at 3'-end ",
-            "c) Free hydroxyl group at 5'-end  ",
-            'd) Both B & C are correct ',
+            "Free phosphate moiety at 5'-end  ",
+            "Free phosphate moiety at 3'-end ",
+            "Free hydroxyl group at 5'-end  ",
+            'Both B & C are correct ',
         ],
         answer: 1,
     },
     {
         question: '"Beads on string" shows?',
         options: [
-            'a) DNA details  ',
-            'b) Histone details ',
-            'c) Chromatin details  ',
-            'd) Nucleotide details ',
+            'DNA details  ',
+            'Histone details ',
+            'Chromatin details  ',
+            'Nucleotide details ',
         ],
         answer: 3,
     },
     {
         question: 'The number of DNA in chromosome at G2 stage of cell cycle?',
-        options: ['a) One  ', 'b) Two ', 'c) Four  ', 'd) Eight '],
+        options: ['One  ', 'Two ', 'Four  ', 'Eight '],
         answer: 2,
     },
     {
         question:
             'Atropine is a medication to treat pesticide poisonings as well as slow heart rate; it is a/an?',
         options: [
-            'a) Glucoside  ',
-            'b) Alkaloid ',
-            'c) Terpenoides  ',
-            'd) Lectins ',
+            'Glucoside  ',
+            'Alkaloid ',
+            'Terpenoides  ',
+            'Lectins ',
         ],
         answer: 2,
     },
@@ -270,10 +208,10 @@ var quiz = [
         question:
             'Hershey & Chase finally proved that the Genetic Material is DNA, They performed experiment on?',
         options: [
-            'a) Mycophage  ',
-            'b) Streptococcus ',
-            'c) Coliphage  ',
-            'd) Fission yeast ',
+            'Mycophage  ',
+            'Streptococcus ',
+            'Coliphage  ',
+            'Fission yeast ',
         ],
         answer: 3,
     },
@@ -281,12 +219,244 @@ var quiz = [
         question:
             'Which enzyme can be used for template independent enzymatic synthesis of RNA?',
         options: [
-            'a) RNA Polymerase  ',
-            'b) DNA Polymerase ',
-            'c) Adenylate Cyclase  ',
-            'd) Polynucleotide Phosphorylase ',
+            'RNA Polymerase  ',
+            'DNA Polymerase ',
+            'Adenylate Cyclase  ',
+            'Polynucleotide Phosphorylase ',
         ],
         answer: 4,
     },
+    {
+        question: 'Which one of the following is a base analogue?',
+        options: [
+            'Nitrous acid  ',
+            'Colchicine ',
+            '5 - bromouracil  ',
+            'Caffeine ',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Isolation and purification of specific DNA segment from a living organism was achieved by?',
+        options: [
+            'Crick  ',
+            'Nirenberg ',
+            'Khorana  ',
+            'Beckwith and his colleagues ',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Feulgen reaction is a special test for?',
+        options: ['RNA  ', 'DNA ', 'Protein  ', 'Carbohydrate '],
+        answer: 2,
+    },
+    {
+        question: 'There is no DNA in?',
+        options: [
+            'An enucleated ovum  ',
+            'Mature RBCs ',
+            'A mature spermatozoan  ',
+            'Hair root ',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Semiconservative model of DNA repilication was proposed by which workers in eukaryotes?',
+        options: [
+            'Taylor, Woods and Hughes, 1957  ',
+            'Messelson and Stahl, 1957 ',
+            'Nirenberg and Khorana, 1967  ',
+            'Watson and Crick, 1952 ',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Semiconservative replication of DNA was first demonstrated in?',
+        options: [
+            'Drosophila melanogaster  ',
+            'Escherichia coli ',
+            'Streptococcus pneumoniae  ',
+            'Salmonella typhimurium ',
+        ],
+        answer: 2,
+    },
+    {
+        question: ' One turn of the helix in a B-form DNA is approximately?',
+        options: ['3.4 nm  ', '2 nm ', '20 nm  ', '0.34 nm '],
+        answer: 1,
+    },
+    {
+        question: 'A-DNA is?',
+        options: [
+            'Left handed helix with 12 nucleotide pair per turn  ',
+            'Right handed helix with 11 nucleotide pairs per turn ',
+            'Right handed helix with 12 nucleotide pairs per turn ',
+            'Left handed helix with 11 nucleotide pairs per turn ',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'In the double helix model of DNA, how far is each base pair from the next base pair?',
+        options: ['3.4 nm  ', '0.34 nm ', '2.0 nm  ', '34 nm '],
+        answer: 2,
+    },
+    {
+        question: 'In the DNA molecule?',
+        options: [
+            'The proportion of Adenine in relation to thymine varies with the organism ',
+            "There are two strands which run antiparallel-one in 5' →3' direction and other in 3'→5' ",
+            'The total amount of purine nucleotides and pyrimidine nucleotides is not always equal ',
+            "There are two strands which run parallel in the 5'→ 3' direction ",
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'A DNA molecule in which both strands have radioactive thymidine is allowed to duplicate in an environment containing non-radioactive thymidine. What will be the correct number of DNA molecule that contain some radioactive thymidine after three duplications?',
+        options: [
+            'There will be four such molecules  ',
+            'There will be eight such molecules ',
+            'There will be only one such molecule  ',
+            'There will be two such molecules ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'In a 34 nm long piece of DNA, 56 adenine bases were found. What would be the number of cytosine bases?',
+        options: ['56  ', '68 ', '72  ', '44 '],
+        answer: 4,
+    },
+    {
+        question:
+            'A 340 Å long segment of DNA molecules has 20 thymine nitrogenous bases, what will be the number of guanine nitrogen bases in the same segment?',
+        options: ['10  ', '40 ', '80  ', '160 '],
+        answer: 3,
+    },
+    {
+        question:
+            'The method developed by Matthew Meselson and Franklin Stahl to separate heavy DNA with 15N from DNA with 14N, for providing evidence for semi-conservative replication of DNA is?',
+        options: [
+            'Ion exchange chromatography  ',
+            'Paper chromatography ',
+            'Buoyant density centrifugation  ',
+            'Gel filtration ',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The haploid content of human DNA is?',
+        options: [
+            '3.3 × 10<sup>9</sup>bp  ',
+            '3.3 × 10<sup>6</sup>kbp ',
+            '4.6 × 10<sub>6</sub>bp  ',
+            '48502 bp ',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'True replication of DNA is possible due to?',
+        options: [
+            'Hydrogen bonding  ',
+            'Phosphate backbone ',
+            'Complementary base pairing rule  ',
+            'None of the above ',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Melting of DNA at an elevated temperature (80℃) is primarily due to the breakdown of?',
+        options: [
+            'Phosphodiester bonds  ',
+            'Glycosidic bonds ',
+            'Disulphide  ',
+            'Hydrogen bonds ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The part of DNA molecule that varies among DNA moelcule is?',
+        options: [
+            'Phosphate molecule  ',
+            'Nitrogen base ',
+            'Sugar molecule  ',
+            'All of these ',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Ribozyme is',
+        options: [
+            'RNA without sugar  ',
+            'RNA without phosphate ',
+            'RNA having enzymic activity  ',
+            'RNA with extra phosphate ',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Watson and Crick are known for their discovery that DNA?',
+        options: [
+            'Is a single stranded helix  ',
+            'Contains deoxyribose only ',
+            'Is a double stranded helix  ',
+            'Synthesizes rRNA ',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Phosphorus is present in?',
+        options: [
+            'Protein  ',
+            'DNA ',
+            'RNA  ',
+            'Both DNA and RNA ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The enzyme which can cut molecules of DNA into segments is known as?',
+        options: [
+            'DNA polymerase  ',
+            'DNA ligase ',
+            'Restriction enzyme  ',
+            'DNA gyrase ',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'DNA consists of two complementary nucleotide chains. If the sequence of nucleotide in one of the chains is AGCTTCGA, then the nucleotide sequence in the other chain shall be?',
+        options: [
+            'TAGCATAT  ',
+            'GATCCTAG ',
+            'TCGAAGCT  ',
+            'GCTAAGCT ',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'If there are 120 adenine molecules in a bDNA double helical structure showing 20 coils, what is the number of pyrimidine nucleotides forming three hydrogen bonds in it?',
+        options: ['80  ', '100 ', '120  ', '140 '],
+        answer: 1,
+    },
+    {
+        question:
+            'Enzyme that cleaves nucleic acids within the polynucleotide chain is known as?',
+        options: [
+            'Ligases  ',
+            'Polymerases ',
+            'Endonucleases  ',
+            'Transcriptases ',
+        ],
+        answer: 3,
+    }
 ];
-
