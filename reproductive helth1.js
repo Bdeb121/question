a2c060dabac4b9f99eb8ff2145165f9c62aa8e4f
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-03 00:08:43 +0530

    Create reproductive helth1.js

---
+++ reproductive helth1.js
@@ -0,0 +1,226 @@
+let quiz = [
+    {
+        question: 'STD may be due to infection of?',
+        options: [
+            'Viruses  ',
+            'Bacteria ',
+            'Protozoa  ',
+            'Any of these ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'In which age group the incidences of sexually transmitted diseases are very high?',
+        options: [
+            'Elderly above sixty  ',
+            'Mid age around fourties ',
+            'Teenagers & youngsters  ',
+            'Kids below twelve years ',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Saheli is a?',
+        options: [
+            'A Diaphragm  ',
+            'Surgical method ',
+            'IUD  ',
+            'Oral Contraceptive ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'What is LNG - 20?',
+        options: [
+            'Oral contraceptive pill  ',
+            'Emergency contraceptive pill ',
+            'Spermicidal Jelly  ',
+            'Hormone releasing IUD ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which is correctly matched?',
+        options: [
+            'Multiload 375 - Non medicated IUD  ',
+            'Lippes loop - Copper releasing IUD ',
+            'Progestasert - Hormone based IUD  ',
+            'All are correctly matched ',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Condom "Nirodh" is manufactured by?',
+        options: [
+            'Hindustan Lever  ',
+            'Hindustan latex ',
+            'IISc  ',
+            'DRDO ',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Ormeloxifene is a?',
+        options: [
+            'Selective estrogen receptor modulator (SERM)  ',
+            'Intra uterine devise ',
+            'Emergency contraceptive  ',
+            'All of these ',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which method is best to have a child if female have healthy uterus but unable to produce egg?',
+        options: ['GIFT  ', 'IVF ', 'AI  ', 'IUI '],
+        answer: 1,
+    },
+    {
+        question:
+            'Which metal ion released by some of the Intra uterine devises to suppress sperm motility & their fertilising capacity?',
+        options: ['Ca++  ', 'Na+ ', 'Fe+++  ', 'Cu+ '],
+        answer: 4,
+    },
+    {
+        question:
+            'A couple want atleast 3 years gap between two childrens. They already have one child. What would be best method for contraception for them?',
+        options: [
+            'Vasectomy  ',
+            'Coitus interruptus ',
+            'IUDs  ',
+            'Periodic abstinence ',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which technique involves the direct injection of sperm into eggs obtained from in vitro fertilization (IVF)?',
+        options: ['ZIFT  ', 'GIFT ', 'ICSI  ', 'IUI '],
+        answer: 3,
+    },
+    {
+        question:
+            "The term 'Health' is defined in many ways. The most accurate definition of the health would be?",
+        options: [
+            'Health is the state of body and mind in a balanced condition ',
+            'Health is the reflection of a smilling face ',
+            'Health is a state of conplete physical, mental and social well-being ',
+            'Health is the symbol of economic prosperity ',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Increased IMR and decreased MMR in a population will?',
+        options: [
+            'Cause rapid increase in growth rate  ',
+            'Result in decline in gorwth rate ',
+            'Not cause significant change in growth rate  ',
+            'Result in an explosive population/exp ',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'A stable population is one which has got?',
+        options: [
+            'No growth of population in the last decade  ',
+            'Constant rate of growth of population in the last decade ',
+            'Slow growth of population in the last decade  ',
+            'Growth of 20% in excess than the last decade ',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Now-a-days India is under which stage of demographic cycle?',
+        options: [
+            'Early expanding  ',
+            'Late expanding ',
+            'Phase of decline  ',
+            'High stationary ',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            "If India's population is increasing at the rate of 13.6 million per year, then what percentage of total increase in world's population in one clender year is contributed by the Indians?",
+        options: ['10%  ', '17% ', '15%  ', '20% '],
+        answer: 2,
+    },
+    {
+        question: 'The age groups in a population are classified by?',
+        options: [
+            'Reproduction rate  ',
+            'Death rate ',
+            'Age of marriage  ',
+            'Sex ratio ',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'One of these causes nongonococcal urethritis?',
+        options: [
+            'Treponema pallidum  ',
+            'Neisseria gonorrhoeae ',
+            'Chlamydia trachomatis  ',
+            'HSV-I ',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Genital warts are due to sexually transmitted disease, spread by?',
+        options: [
+            'Herpes virus  ',
+            'Papilloma virus ',
+            'Hepatitis A  ',
+            'Trichomonas ',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Which of these is used to control human population?',
+        options: [
+            'Estrogen + progesterone  ',
+            'IUCD and MTP ',
+            'Tubectomy and Vasectomy  ',
+            'All of these ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Assited reproductive technology, IVF involves transfer of?',
+        options: [
+            'Zygote into uterus  ',
+            'Embryo with 16 blastomeres into fallopian tube ',
+            'Ovum into the fallopian tube  ',
+            'Zygote into the fallopian tube ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Surgical removal of testes is known as?',
+        options: [
+            'Testectomy  ',
+            'Gonadectomy ',
+            'Castration  ',
+            'None ',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'The success of birth control programmes in controlling population growth is dependent on?',
+        options: [
+            'Use of contraceptives  ',
+            'Tubectomy ',
+            'Vasectomy  ',
+            'Acceptability of the above the people ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Trade name of weekly oral contraceptive pill is',
+        options: ['Mala  ', 'Saheli ', 'Mala A  ', 'Mala D '],
+        answer: 2,
+    }
+];
