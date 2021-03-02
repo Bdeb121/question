e9fa66ab2fc8e77165b032593e504dc0ea194816
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-03 00:11:15 +0530

    Create reproductive helth2.js

---
+++ reproductive helth2.js
@@ -0,0 +1,308 @@
+let quiz = [
+    {
+        question: 'RCH stands for?',
+        options: [
+            'Rotine check-up of health  ',
+            'Reproduction cum hygiene ',
+            'Reversible contraceptive hazards  ',
+            'Reproductive and child health care ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'For imposing a check on increasing female foeticides?',
+        options: [
+            'MTP has been legalized  ',
+            'Tubectomy is being practiced ',
+            'Amniocentesis has been banned  ',
+            'All of these ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            "Present increase in India's population has not been due to decline in?",
+        options: [
+            'Infant mortality rate  ',
+            'Number of people reaching reproductive age ',
+            'Death rate  ',
+            'Maternal mortality rate ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The copper ions of IUDs?',
+        options: [
+            'Suppress motility and the fertilising capacity of sperms. ',
+            'Make the urerus unsuitable for implantation ',
+            'Increase phagocytosis of sperms  ',
+            'Make cervix hostile to sperms ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Progestasert and LNG-20 are?',
+        options: [
+            'Implants  ',
+            'Copper releasing IUDs ',
+            'Non-medicated IUDs  ',
+            'Hormone releasing IUDs ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Tying up or removing a small part of fallopian duct is called?',
+        options: [
+            'Vasectomy  ',
+            'Ductus arteriosus ',
+            'Archidectomy  ',
+            'Tubectomy ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Read the following 4-statements and mark the option that has correct statements ',
+        statement: [
+            'A. MTP was legalized in 1971  ',
+            'B. Inability to conceive or produce children even after 2 years of unprotected sexual cohabitation is called infertility  ',
+            'C. Surgical method of contraception prevents gamete formation  ',
+            'D. MTPs are relatively safe up to 12 weeks of pregnancy ',
+        ],
+        options: [
+            'A, B and D  ',
+            'B, C and D ',
+            'C and D  ',
+            'A and C ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The family planning programme in India were initiated in?',
+        options: ['1951  ', '1961 ', '1971  ', '1981 '],
+        answer: 4,
+    },
+    {
+        question: 'The diaphragm, cervical cap and vaults are?',
+        options: [
+            'Disposable contraceptive devices  ',
+            'Reusable contraceptives ',
+            'IUDs  ',
+            'Implants ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'For delaying pregnancy or spacing children the ideal contraceptive is?',
+        options: [
+            'Vasectomy  ',
+            'Tubectomy ',
+            'IUD  ',
+            'Oral contraceptive ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The transfer of zygote or early embryo (up to 8 blastomere) into fallopian tube is?',
+        options: ['IUF and ET  ', 'ZIFT ', 'GIFT  ', 'IUT '],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following statements is wrong?',
+        options: [
+            'Test tube baby grows in test tube following uterine fertilization till complete gestation. ',
+            "Test tube baby grows within mother's womb. ",
+            "Test tube grows within surrogate mother's womb.  ",
+            'Test tube baby grows inside test tube in initial embryogenesis ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'What is the figure given below showing in particular?',
+        options: [
+            'Ovarian cancer  ',
+            'Uterine cancer ',
+            'Tubectomy  ',
+            'Vasectomy ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The test-tube Baby Programme employs which one of the following techniques?',
+        options: [
+            'Intra cytoplasmic sperm injection (ICSI)  ',
+            'Intra uterine insemination (IUI) ',
+            'Gamete intra fallopian transfer (GIFT)  ',
+            'Zygote intra fallopian transfer (ZIFT) ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which of the following cannot be detected in a developing foetus by amniocentesis?',
+        options: [
+            'Sex of the foetus  ',
+            'Down syndrome ',
+            'Jaundice  ',
+            'Klinefelter syndrome ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Artificial insemination means?',
+        options: [
+            'Transfer of sperms of husband to a test tube containing ova ',
+            'Artificial introduction of sperms of healthy donor into the vagina ',
+            'Introduction of sperms of healthy donor directly into the ovary ',
+            'Transfer of sperms of healthy donor to a test tube containing ova ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which of the following is a hormone releasing Intra Uterine Device (IUD)?',
+        options: [
+            'Multiload 375  ',
+            'LNG - 20 ',
+            'Cervical cap  ',
+            'Vault ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Assisted reproductive technology, IVF involves transfer of?',
+        options: [
+            'Ovum into the fallopian tube.  ',
+            'Zygote into the fallopian tube. ',
+            'Zygote into the uterus.  ',
+            'Embryo with 16 blastomeres into the fallopian tube. ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'A contraceptive pill prevents ovulation by?',
+        options: [
+            'Blocking Fallopian tube  ',
+            'Inhibiting release of FSH and LH ',
+            'Stimulating release of FSH and LH  ',
+            'Causing immediate degeneration of released ovum ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Progestasert is an IUD which makes the uterus unsuitable and cervix hostile to the sperms as they are?',
+        options: [
+            'Hormone releasing IUDs  ',
+            'Copper releasing IUDs ',
+            'Ideal contraceptive  ',
+            'Non-medicated IUDs ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Assisted Reproductive Technology does not include?',
+        options: [
+            'Zygote Extra Fallopian Transfer  ',
+            'In vitro fretilization and embryo transfer ',
+            'Artificial insemination  ',
+            'Gamete Intra Fallopian Transfer ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'How many of the following statement is/ are correct? ',
+        statement: [
+            'I. MTP has a significant role in population decrease.  ',
+            'II. Government of India legalized MTP in 1951.  ',
+            'III. MTP is considered safer during second trimester.  ',
+            'IV. 45-50 billion MTP are performed in world in an year. ',
+        ],
+        options: ['One  ', 'Two ', 'Three  ', 'Four '],
+        answer: 4,
+    },
+    {
+        question:
+            'In contex of Amniocentesis, which of the following statement is legally incorrect?',
+        options: [
+            'It is usually done when a women is between 14-16 weeks pregnant. ',
+            'It is used for prenatal sex determination ',
+            'It can be used for detection of Down syndrome  ',
+            'It can be used for detection of chromosomal anomalies ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is hormone releasing IUD?',
+        options: [
+            'Lippes loop  ',
+            'Cu7 ',
+            'LNG-20  ',
+            'Multiload 375 ',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is incorrect regarding vasectomy?',
+        options: [
+            'Vasa deferentia is cut and tied  ',
+            'Irreversible sterility ',
+            'No sperm occurs in seminal fluid  ',
+            'No sperm occurs in epididymis ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Embryo with more than 16 blastomeres formed due to in vitro fertilization is transferred into?',
+        options: [
+            'Fimbriae  ',
+            'Cervix ',
+            'Uterus  ',
+            'Fallopian tube ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Transfer of an ovum of a donor into the fallopian tube of a surrogate mother is?',
+        options: ['ET  ', 'IUT ', 'GIFT  ', 'ZIFT '],
+        answer: 4,
+    },
+    {
+        question: "The function of copper ions in copper releasing IUD's is?",
+        options: [
+            'They inhibit gametogenesis  ',
+            'They make uterus unsuitable for implantation ',
+            'They inhibit ovulation  ',
+            'The suppress sperm motility & fertilizing capacity of sperms ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'In case of a couple where the male is having a very low sperm count, which technique will be suitable for fertilization?',
+        options: [
+            'Gamete intracytoplasmic fallopian transfer  ',
+            'Artificial Insemination ',
+            'Intracytoplasmic sperm injection  ',
+            'Intrauterine transfer ',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which of the following statement is wrong about test tube baby?',
+        options: [
+            'The embryo more than eight blastomeres is transferred into the uterus ',
+            'The ova and sperms used in this technique are obtained only from wife and husband only ',
+            'Fusion of sperm and ovum is done outside the body of female ',
+            'The zygote or early embryo upto eight blastomeres is transferred into the fallopian tube ',
+        ],
+        answer: 4,
+    }
+];
