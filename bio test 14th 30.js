3bfd08fd688a82bcc70da5e145fe743515cfe56b
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-05 23:39:32 +0530

    Create bio test 14th 30.js

---
+++ bio test 14th 30.js
@@ -0,0 +1,476 @@
+let quiz = [
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Lal Bagh botanical garden ',
+                'National Botanical Garden ',
+                'Indian Botanical Garden ',
+                'Forest Research Institute ',
+                'Lloyd Botanical Garden ',
+            ],
+            right: [
+                'Darjeeling',
+                'Kolkata',
+                'Lucknow',
+                'Dehradun',
+                'Bengaluru',
+            ],
+        },
+        options: [
+            'a-5 b-3 c-2 d-4 e-1',
+            'a-3 b-2 c-4 d-2 e-1',
+            'a-1 b-3 c-4 d-2 e-5',
+            'a-1 b-3 c-4 d-5 e-2',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Herbarium ',
+                'Botanical Garden',
+                'Zoological Park',
+                'Museum',
+            ],
+            right: [
+                'Preserved plant and animal specimen and also artistic and educational material',
+                'Living wild animal in their natural habitat',
+                'Preserved plant specimen on sheet',
+                'Diversity of living plant',
+            ],
+        },
+        options: [
+            'a-3 b-4 c-2 d-1',
+            'a-1 b-4 c-2 d-1',
+            'a-4 b-3 c-1 d-2',
+            'a-4 b-1 c-2 d-3',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Kingdom',
+                'Order',
+                'Family ',
+                'Genus ',
+                'Species ',
+            ],
+            right: [
+                'Annuus',
+                'Helianthus',
+                'Fungi',
+                'Canidae',
+                'Rannales',
+            ],
+        },
+        options: [
+            'a-3 b-5 c-4 d-2 e-1',
+            'a-3 b-4 c-5 d-2 e-1',
+            'a-3 b-5 c-2 d-4 e-1',
+            'a-1 b-5 c-2 d-4 e-1',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Planaria ',
+                'Fungi ',
+                'Yeast ',
+                'Hydra ',
+                'Amoeba ',
+            ],
+            right: [
+                'Binary fission',
+                'Asexual spores',
+                'Budding',
+                'True regeneration',
+                'Fragmentation',
+            ],
+        },
+        options: [
+            'a-1 b-2 c-4 d-5 e-1',
+            'a-4 b-2 c-3 d-3 e-1',
+            'a-3 b-2 c-3 d-1 e-1',
+            'a-4 b-2 c-1 d-3 e-1',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Morels ',
+                'Smut ',
+                'Bread mould ',
+                'Alternaria ',
+            ],
+            right: [
+                'Deuteromycetes',
+                'Ascomycetes',
+                'Basidiomycetes',
+                'Phycomycetes',
+            ],
+        },
+        options: [
+            'a-3 b-4 c-1 d-2',
+            'a-2 b-3 c-4 d-1',
+            'a-4 b-1 c-2 d-3',
+            'a-3 b-4 c-2 d-1',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Blast disease of rice',
+                'Citrus canker',
+                'Grain smut of 𝑆𝑜𝑟𝑔ℎ𝑢𝑚',
+                'Red rot of sugarcane',
+            ],
+            right: [
+                'Dikaryotic mycelium',
+                'Single-celled conidiophores',
+                'Gram positive bacteria',
+                'Septate conidiophores',
+                'Gram negative bacteria',
+            ],
+        },
+        options: [
+            'a-4 b-5 c-3 d-2',
+            'a-4 b-5 c-1 d-2',
+            'a-2 b-3 c-5 d-1',
+            'a-2 b-3 c-1 d-5',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: ['Coccus', 'Bacillus', 'Vibrio', 'Spirillum'],
+            right: [
+                'Rod-shaped',
+                'Spherical',
+                'Spiral-shaped',
+                'Comma-shaped',
+            ],
+        },
+        options: [
+            'a-3 b-2 c-1 d-4',
+            'a-4 b-3 c-2 d-1',
+            'a-2 b-1 c-4 d-3',
+            'a-1 b-4 c-3 d-2',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Two kingdom system of classification',
+                'Five kingdom system of classification',
+                'Six kingdom system of classification',
+            ],
+            right: [
+                'RH Whittaker',
+                'Carl Woese',
+                'Carolus Linnaeus',
+            ],
+        },
+        options: ['a-2 b-1 c-3', 'a-1 b-2 c-4', 'a-4 b-3 c-1', 'a-3 b-1 c-2'],
+        answer: 4,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'D.J. Ivanowky (1892)',
+                'M.W. Beijerinck (1898)',
+                'W.M. Stanley (1935)',
+                'T. O. Diener (1971)',
+            ],
+            right: [
+                'Viroids',
+                'First crystallized TMV',
+                'Contagium vivum fluidum',
+                'Mosaic disease of tobacco',
+            ],
+        },
+        options: [
+            'a-1 b-4 c-3 d-2',
+            'a-4 b-3 c-2 d-1',
+            'a-2 b-1 c-4 d-3',
+            'a-3 b-2 c-1 d-4',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Obelia',
+                'Meandrina',
+                'Gorgonia',
+                'Physalia',
+            ],
+            right: [
+                'Portugese man of war',
+                'Sea fan',
+                'Sea anemone',
+                'brain coral',
+                'Sea fur',
+            ],
+        },
+        options: [
+            'a-5 b-3 c-2 d-4',
+            'a-1 b-2 c-4 d-5',
+            'a-5 b-4 c-2 d-1',
+            'a-3 b-1 c-2 d-4',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Phylum-Ctenophora',
+                'Phylum-Annelida',
+                'Phylum-Platyhelminthes',
+                'Phylum-Aschelminthes',
+            ],
+            right: [
+                'Flame cells',
+                'Longer females',
+                'Coelomate',
+                'Comb plates',
+            ],
+        },
+        options: [
+            'a-3 b-4 c-2 d-1',
+            'a-4 b-3 c-1 d-2',
+            'a-4 b-1 c-2 d-3',
+            'a-2 b-1 c-4 d-3',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Honey bee',
+                'Mosquito',
+                'Laccifer',
+                'Bombyx',
+            ],
+            right: ['Aedes', 'Apis', 'Lac insect', 'Silkworm'],
+        },
+        options: [
+            'a-1 b-2 c-3 d-4',
+            'a-3 b-1 c-2 d-4',
+            'a-2 b-1 c-3 d-4',
+            'a-4 b-1 c-3 d-2',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: ['', '', '', ''],
+            right: [
+                'scoliodon',
+                'pristis',
+                'myxine',
+                'Catla',
+                'Petromyzon',
+            ],
+        },
+        options: [
+            'a-4 b-2 c-5 d-1',
+            'a-4 b-2 c-3 d-1',
+            'a-1 b-3 c-5 d-2',
+            'a-1 b-4 c-5 d-3',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Green glands',
+                'Amphids and phasmids',
+                'Ctenidia',
+                'Poison claw',
+                'Concholin',
+            ],
+            right: [
+                'centipede',
+                'Respiratory organ',
+                'Shell protein',
+                'Excretory organs',
+                'Sense organs',
+            ],
+        },
+        options: [
+            'a-4 b-5 c-2 d-1 e-3',
+            'a-1 b-3 c-4 d-5 e-3',
+            'a-2 b-4 c-5 d-3 e-3',
+            'a-3 b-4 c-5 d-2 e-3',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Hippocampus',
+                'delphinus',
+                'Salamander',
+                'struthrio',
+            ],
+            right: [
+                'Dicondylic skull',
+                'Can regenerate it limbs',
+                'Air sac in lungs',
+                'Have Amnion and Chorion',
+                'Have lateral line sense organs',
+            ],
+        },
+        options: [
+            'a-1 b-2 c-3 d-4',
+            'a-4 b-2 c-1 d-3',
+            'a-3 b-2 c-1 d-4',
+            'a-5 b-1 c-2 d-3',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Sphaerosomes',
+                'Peroxisomes',
+                'Plasmodesmata',
+                'Nucleolus',
+            ],
+            right: [
+                'RNA synthesis',
+                'Lipid storage',
+                'Glycolate metabolism',
+                'Transport of macromolecules',
+            ],
+        },
+        options: [
+            'a-2 b-3 c-4 d-1',
+            'a-3 b-4 c-1 d-2',
+            'a-1 b-2 c-3 d-4',
+            'a-4 b-1 c-2 d-3',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Chargaff',
+                'Replicon',
+                'Permease',
+                'Split gene',
+                'x-ray diffraction studies',
+            ],
+            right: [
+                'Wilkins and Franklin',
+                'Uptake of lactose',
+                'hn RNA',
+                'Length of DNA',
+                '(A + G)=(C + T)',
+            ],
+        },
+        options: [
+            'a-5 b-4 c-2 d-3 e-1',
+            'a-5 b-4 c-1 d-3 e-1',
+            'a-5 b-4 c-2 d-1 e-1',
+            'a-5 b-1 c-2 d-3 e-1',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Leeuwenhoek Described a living cell',
+                'Robert Hooke Unicellular',
+                'Chlamydomonas Discovered cell membrane',
+                'Robert Brown Wrote ‘Micrographia’',
+                'Schwann Discovered the nucleus',
+            ],
+            right: [
+                'Leeuwenhoek Described a living cell',
+                'Robert Hooke Unicellular',
+                'Chlamydomonas Discovered cell membrane',
+                'Robert Brown Wrote ‘Micrographia’',
+                'Schwann Discovered the nucleus',
+            ],
+        },
+        options: [
+            'a-1 b-4 c-2 d-5 e-3',
+            'a-3 b-2 c-5 d-4 e-3',
+            'a-4 b-1 c-5 d-3 e-3',
+            'a-5 b-3 c-1 d-4 e-3',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Endoplasmic reticulum',
+                'Free ribosome',
+                'Mitochondrion',
+                'Contractile vacuole',
+            ],
+            right: [
+                'Take part in cellular respiration',
+                'Take part in osmoregulation and excretion',
+                'Synthesis of lipids',
+                'Synthesise non-secretary proteins',
+            ],
+        },
+        options: [
+            'a-3 b-4 c-1 d-2',
+            'a-1 b-2 c-4 d-3',
+            'a-3 b-2 c-1 d-4',
+            'a-3 b-2 c-4 d-1',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Match the following columns',
+        table: {
+            left: [
+                'Zygote',
+                'Cork cambium',
+                'Large cells',
+                'Small cells',
+            ],
+            right: [
+                'Dedifferentiation',
+                'Higher surface volume ratio',
+                'Undifferentiated cell',
+                'Lower surface volume ratio',
+            ],
+        },
+        options: [
+            'a-4 b-2 c-1 d-3',
+            'a-3 b-1 c-4 d-2',
+            'a-2 b-3 c-1 d-4',
+            'a-1 b-4 c-2 d-3',
+        ],
+        answer: 2,
+    }
+]
