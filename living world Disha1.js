941e2f52a2eff75525721daeb5ec0d2cb9a55c75
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-17 21:48:31 +0530

    Create living world Disha1.js

---
+++ living world Disha1.js
@@ -0,0 +1,301 @@
+let quiz = [
+    {
+        question:
+            'Which is the most important but generally not used criteria for the identification of the species ?',
+        options: [
+            'Interbreeding ',
+            'Morphology',
+            'Genetic material ',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The living organisms can be unexceptionally distinguished from the non-living things on the basis of their ability for',
+        options: [
+            'interaction with the environment and progressive evolution',
+            'reproduction',
+            'growth and movement',
+            'responsiveness to touch',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'iological organisation starts with',
+        options: [
+            'cellular level',
+            'organism level',
+            'atomic level',
+            'sub-microscopic molecular level',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The growth and reproduction are mutually exclusive events in',
+        options: [
+            'Plants only',
+            'Animals only',
+            'Higher animals and plants',
+            'Lower organisms',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'The sum total of chemical reactions occurring in our body is called',
+        options: [
+            'Metabolism ',
+            'Homeostasis',
+            'Irritability ',
+            'Catabolism',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'ark the correct statement',
+        options: [
+            'Only living organisms grow.',
+            'Plants grow only up to a certain age.',
+            'The growth in living organisms is from inside.',
+            'All of these',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which of the following biological processes do not operate within the life span of a given organism ?',
+        options: [
+            'Birth and nutrition',
+            'Growth and maturation',
+            'Metabolism and excretion',
+            'Decomposition and mineralization',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Growth development and functioning of living body is due to -',
+        options: [
+            'Order ',
+            'Homeostasis',
+            'Metabolism ',
+            'Adaptation',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which type of organisation is found in only living beings ?',
+        options: [
+            'Atomic ',
+            'Molecular',
+            'Mixture ',
+            'Sub-cellular',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Organisation levels in living beings are -',
+        options: [
+            'Subcellular – cellular – individual – community – population',
+            'Atomic – molecular – subcellular – cellular – tissue – organ – individual',
+            'Individual – population – organ system – tissue – cellular – molecular – atomic',
+            'Atomic – molecular – tissue – individual – ecosystem – community.',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Which of the following statement is false?',
+        options: [
+            'Properties of cellular organelles are present in the molecular constituents of the organelles.',
+            'Interactions among the molecular components of the organelles result into properties of cell organelles.',
+            'Biology is the story of life on earth.',
+            'Biology is the story of evolution of living organisms on earth.',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'A plant differs from an animal mainly in :',
+        options: [
+            'Protoplasm ',
+            'Vital activities',
+            'Nutrition ',
+            'Reproduction',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Artificial system of classification classifies plants on the basis of –',
+        options: [
+            'One or two characters',
+            'Phylogenetic trends',
+            'Many naturally existing characters',
+            'None of the above',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Group of organisms that closely resemble each other and freely interbreed in nature, constitute a–',
+        options: ['Species ', 'Genus', 'Family ', 'Taxon'],
+        answer: 1,
+    },
+    {
+        question: 'ICBN was first revised in –',
+        options: ['1961 ', '1964', '1975 ', '1753'],
+        answer: 3,
+    },
+    {
+        question: 'The term taxon refers to –',
+        options: [
+            'Name of a species',
+            'Name of genus',
+            'Name of family',
+            'A taxonomic group of any rank',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The herbarium specimen on whose basis a new species is described for the first time is called as –',
+        options: [
+            'Syntype ',
+            'Holotype',
+            'Paratype ',
+            'Neotype',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'The scientific naming of plants began with publication of Linnaeus book –',
+        options: [
+            'Genera plantarum ',
+            'Systema naturae',
+            'Species plantarum ',
+            'Charaka sanhita',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The basic unit of classification is –',
+        options: [
+            'Genus ',
+            'Species',
+            'Order ',
+            'All of these',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Individuals of same species having non-genetic differences due to environment are called –',
+        options: [
+            'Biotypes ',
+            'Ecotype',
+            'Ecophenes ',
+            'None of these',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Morphologically similar but reproductively isolated species are called –',
+        options: [
+            'Neontological species ',
+            'Sibling species',
+            'Allopatric species ',
+            'Morpho-species',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Plant nomenclature means :',
+        options: [
+            'To give names to plants without any rules',
+            'Nomenclature of plants under the international rules',
+            'Nomenclature of plants in local language',
+            'Nomenclature of plants in english language',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Taxonomy refers to –',
+        options: [
+            'Plant classification ',
+            'Plant nomenclature',
+            'Plant affinity ',
+            'All of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is a correct name ?',
+        options: [
+            'Solanum tuberosum',
+            'Solanum Tuberosum',
+            'Solanum tuberosum Linn.',
+            'All of these',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Systematics deals with –',
+        options: [
+            'Classification ',
+            'Nomenclature',
+            'Plant description ',
+            'Plant exploration',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Phylogeny refers to –',
+        options: [
+            'Natural classification',
+            'Evolutionary classification',
+            'Evolutionary history',
+            'Origin of algae',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Static concept of species is given by –',
+        options: ['Linnaeus ', 'Bentham', 'Koch ', 'Mayr'],
+        answer: 1,
+    },
+    {
+        question: 'In taxonomy the first step is :',
+        options: [
+            'Identification ',
+            'Nomenclature',
+            'Classification ',
+            'Affinities',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The suffix - inae signifies the rank :',
+        options: [
+            'Tribe ',
+            'Sub tribe',
+            'Sub order ',
+            'Sub family',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Species living in different geographical areas are called –',
+        options: [
+            'Allochronic ',
+            'Allopatric',
+            'Sympatric ',
+            'Siblings',
+        ],
+        answer: 2,
+    }
+];
