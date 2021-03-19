bda9ef70c25ccef118e62077ee96dd9b0ba4bc78
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-19 18:00:44 +0530

    Create biological classification conceptual3.js

---
+++ biological classification conceptual3.js
@@ -0,0 +1,147 @@
+let quiz = [
+    {
+        question: 'Which one of the following is not true about lichens?',
+        options: [
+            'Their body is composed of both algae and fungal cells.',
+            'These grow very fast at the rate of about 2cm per year.',
+            'Some form food for reindeer in arctic regions.',
+            'Some species can be used as pollution indicators.',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'There exists a close association between the alga and the fungus within a lichen. The fungus',
+        options: [
+            'provides protection, anchorage and absorption for the alga',
+            'provides food for the alga',
+            'fixes the atmospheric nitrogen for the alga',
+            'releases oxygen for the alga',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The thalloid body of a slime mould (Myxomycetes) is known as',
+        options: [
+            'plasmodium ',
+            'fruiting body',
+            'mycelium ',
+            'protonema',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Which pair of the following belongs to Basidiomycetes ?',
+        options: [
+            'Puffballs and Claviceps',
+            'Peziza and stink borns',
+            'Morchella and mushrooms',
+            'Birds nest fungi and puffballs.',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which one of the following is a slime mould?',
+        options: [
+            'Physarum ',
+            'Thiobacillus',
+            'Anabaena ',
+            'Rhizopus',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Thermococcus, Methanococcus and Methanobacterium exemplify:',
+        options: [
+            'Archaebacteria that contain protein homologous to eukaryotic core histones',
+            'Archaebacteria that lack any histones resembling those found in eukaryotes but whose DNA is negatively supercoiled',
+            'Bacteria whose DNA is relaxed or positively supercoiled but which have a cytoskeleton as well as mitochondria',
+            'Bacteria that contain a cytoskeleton and ribosomes',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Mannitol is the stored food in:',
+        options: [
+            'Porphyra ',
+            'Fucus',
+            'Gracillaria ',
+            'Chara',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Single-celled eukaryotes are included in:',
+        options: ['Protista ', 'Fungi', 'Archaea ', 'Monera'],
+        answer: 1,
+    },
+    ,
+    {
+        question: 'Ringworm in humans is caused by :',
+        options: [
+            'Bacteria ',
+            'Fungi',
+            'Nematodes ',
+            'Viruses',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Which one of the following organisms is not an example of eukaryotic cells ?',
+        options: [
+            'Paramecium caudatum ',
+            'Escherichia coli',
+            'Euglena viridis ',
+            'Amoeba proteus',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'In eubacteria, a cellular component that resembles eukaryotic cells is :',
+        options: [
+            'plasma membrane ',
+            'nucleus',
+            'ribosomes ',
+            'cell wall',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Organisms called methanogens are most abundant in a :',
+        options: [
+            'sulphur rock ',
+            'cattle yard',
+            'polluted stream ',
+            'hot spring',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'The highest number of species in the world is represented by',
+        options: ['Fungi ', 'Mosses', 'Algae ', 'Lichens'],
+        answer: 1,
+    },
+    ,
+    {
+        question:
+            'In the five-kingdom classification, Chlamydomonas and Chlorella have been included in',
+        options: ['protista ', 'algae', 'plantae ', 'monera'],
+        answer: 2,
+    },
+    {
+        question:
+            'Which one of the following pathogens causes canker disease ?',
+        options: [
+            'Meloidogyne incognita',
+            'Anguina tritici',
+            'Xanthomonas citri',
+            'Pseudomonas rubilineans',
+        ],
+        answer: 3,
+    },
+];
