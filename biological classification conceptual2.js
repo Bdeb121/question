b71e43b9cf7649ec6560995884ad3331bfbf9c65
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-19 18:00:03 +0530

    Create biological classification conceptual2.js

---
+++ biological classification conceptual2.js
@@ -0,0 +1,304 @@
+let quiz = [
+  {
+        question: 'Gametangial copulation (conjugation) is common in',
+        options: [
+            'Zygomycetes ',
+            'Ascomycetes',
+            'Phycomycetes ',
+            'Deuteromycetes',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Dikaryon formation is characteristic of',
+        options: [
+            'Ascomycetes and basidiomycetes',
+            'Phycomycetes and basidiomycetes',
+            'Ascomycetes and phycomycetes',
+            'Phycomycetes and zygomycetes',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Plasmogamy is fusion of',
+        options: [
+            'Two haploid cells including their nuclei',
+            'Two haploid cells without nuclear fusion',
+            'Sperm and egg',
+            'Sperm and two polar nuclei',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Clamp connection is found in',
+        options: [
+            'Basidiomycetes ',
+            'Ascomycetes',
+            'Saccharomycetes ',
+            'Haplomycetes',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Difference between virus and viroid is',
+        options: [
+            'absence of protein coat in viroid but present in virus',
+            'presence of low molecular weight RNA in virus but absent in viroid',
+            'both a and b',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Common bread mould is',
+        options: ['Yeast ', 'Rhizopus', 'Bacteria ', 'Virus'],
+        answer: 2,
+    },
+    {
+        question: 'Branched, aseptate, coenocytic mycelium is present in',
+        options: [
+            'Aspergillus ',
+            'Albugo',
+            'Penicillium ',
+            'Erysiphae',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'In manufacture of bread, it becomes porous due to release of CO2 by the action of',
+        options: ['Virus ', 'Yeast', 'Bacteria ', 'Protozoans'],
+        answer: 2,
+    },
+    {
+        question: 'Members of phycomycetes are found in',
+        options: [
+            'aquatic habitats ',
+            'on decaying wood',
+            'moist and damp places ',
+            'all of these',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            "40. 'Mycorrhizae' are useful for plants mainly due to their following attribut",
+        options: [
+            'Fixing atmospheric nitrogen',
+            'Enhanced absorption of nutrients from soil',
+            'Killing insects and pathogens',
+            'Providing resistance against abiotic stresses',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Red rot of sugarcane is caused by',
+        options: [
+            'Puccinia ',
+            'Albugo',
+            'Ustilago ',
+            'Colletotrichum',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Fungi are always',
+        options: [
+            'Autotrophs ',
+            'Heterotrophs',
+            'Saprophytes ',
+            'Parasites',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Which of the following option is true for methanogens?',
+        statement: [
+            '(i) They are eukaryotes.',
+            '(ii) They live in marshy areas.',
+            '(iii) They are also present in the guts of several ruminant animals such as cows and buffaloes.',
+            '(iv) They are responsible for the production of methane (biogas)',
+        ],
+        options: [
+            '(i) and (ii) ',
+            '(ii) and (iii)',
+            '(i), (ii), (iii) and (iv) ',
+            'None of these',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Pseudomycelium is characteristic feature of',
+        options: [
+            'Mushroom ',
+            'Mucor',
+            'Bread mould ',
+            'Yeast',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Common form of food stored in a fungal cell is',
+        options: ['Glycogen ', 'Starch', 'Glucose ', 'Sucrose'],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following pigment present in cyanobacteria?',
+        options: [
+            'Chlorophyll ‘a’ ',
+            'Chlorophyll ‘b’',
+            'Chlorphyll ‘c’ ',
+            'Chlorophyll ‘d’',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Rhizopus belongs to the class',
+        options: [
+            'Ascomycetes ',
+            'Phycomycetes',
+            'Basidiomycetes ',
+            'Deuteromycetes',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Which one of the following is smallest living cell and live without oxygen?',
+        options: [
+            'Mycoplasma ',
+            'Mycorrhiza',
+            'Euglena ',
+            'Trypanosoma',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The Tobacco mosaic virus was crystallized for first time by',
+        options: [
+            'W. M. Stanley ',
+            'E . C. Stackman',
+            'A. K. Smith ',
+            'Ivanowski',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The genetic material in viruses is',
+        options: [
+            'Only RNA',
+            'Only DNA',
+            'RNA and DNA both',
+            'RNA or DNA i.e. one nucleic acid in a virus',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which of the following pair comes under the group chrysophytes?',
+        options: [
+            'Diatoms and Euglena',
+            'Euglena and Trypanosoma',
+            'Diatoms and desmids',
+            'Gonyaulax and desmids',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Which one is absent in viruses ?',
+        options: [
+            'Replication ',
+            'Protein synthesis',
+            'Energy liberation ',
+            'Mutation',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Protists obtain food as',
+        options: [
+            'photosynthesisers, symbionts and holotrophs',
+            'photosynthesisers',
+            'chemosynthesisers',
+            'holotrophs',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The part of the virus which gives to it the hereditary feature, is',
+        options: [
+            'Capsid ',
+            'Capsomere',
+            'Nucleic acid ',
+            'Nucleotide',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'A bacteriophage is',
+        options: [
+            'A virus attacking a bacterium',
+            'A bacterium attacking a virus',
+            'A stage in the life-cycle of bacterium',
+            'A virus attacking another virus',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'A few organisms are known to grow and multiply at temperatures of 10105ºC. They belong to',
+        options: [
+            'marine archaebacteria',
+            'thermophilic sulphur bacteria',
+            'hot-spring blue-green algae (cyanobacteria)',
+            'thermophilic, subaerial fungi',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The most abundant prokaryotes helpful to humans in making curd from milk and in production of antibiotics are the ones categorised as :',
+        options: [
+            'Cyanobacteria',
+            'Archaebacteria',
+            'Chemosynthetic autotrophs',
+            'Heterotrophic bacteria',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The cyanobacteria are also referred to as',
+        options: [
+            'protists ',
+            'golden algae',
+            'slime moulds ',
+            'blue green algae',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Lichens are important in studies on atmospheric pollution because they',
+        options: [
+            'can also grow in greatly polluted atmosphere',
+            'can readily multiply in polluted atmosphere',
+            'efficiently purify the atmosphere',
+            'are very sensitive to pollutants',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The symbiotic association of fungi and algae is called',
+        options: [
+            'Lichen ',
+            'Mycorrhiza',
+            'Rhizome ',
+            'Endomycorrhiza',
+        ],
+        answer: 1,
+    }
+];
