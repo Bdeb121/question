0167e71b4e1526d8a8d8f3c31aaaef6787a128e1
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-19 17:58:42 +0530

    Create biological classification conceptual1.js

---
+++ biological classification conceptual1.js
@@ -0,0 +1,286 @@
+let quiz = [
+    {
+        question:
+            "In Whittaker's system of classification, prokaryotes are placed in the kingdom",
+        options: [
+            'Protista ',
+            'Monera',
+            'Plantae ',
+            'Animalia',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'In the five kingdom system of classification, which single kingdom out of the following ',
+        statement: [
+            'can include blue-green algae',
+            'nitrogen fixing bacteria',
+            'methanogenic archaebacteria',
+        ],
+        options: ['Monera ', 'Fungi', 'Plantae ', 'Protista'],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following kingdom does not have nuclear membrane?',
+        options: ['Protista ', 'Fungi', 'Monera ', 'Plantae'],
+        answer: 3,
+    },
+    {
+        question:
+            'What type of mode of nutrition is found in the kingdom Animalia?',
+        options: [
+            'Autotrophic and heterotrophic',
+            'Chemosynthetic and photosynthetic',
+            'Saprophytic and parasitic',
+            'Holozoic and saprophytic',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The separation of living beings into five kingdoms is based on',
+        options: [
+            'Complexity of cell structure',
+            "Complexity of organism's body",
+            'Mode of obtaining nutrition',
+            'All of the above',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The chief component of bacterial cell wall is',
+        options: [
+            'Cellulose and chitin',
+            'Cellulose and pectin',
+            'Amino acids and polysaccharides',
+            'Cellulose and carbohydrates',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Bacteria whose cell has only a curve/comma is',
+        options: ['Vibrio ', 'Cocci', 'Spirilli ', 'Bacilli'],
+        answer: 1,
+    },
+    {
+        question:
+            'The main difference between Gram positive and Gram negative bacteria lies in the composition of',
+        options: [
+            'Cilia ',
+            'Cell wall',
+            'Nucleolus ',
+            'Cytoplasm',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Helically coiled shaped bacteria are called',
+        options: ['Spirilla ', 'Coed', 'Bacilli ', 'Vibrio'],
+        answer: 1,
+    },
+    {
+        question: 'Cell wall is absent in',
+        options: ['bacteria ', 'fungi', 'plants ', 'animals'],
+        answer: 4,
+    },
+    {
+        question:
+            'Which one of the following organisms may respire in the absence of oxygen ?',
+        options: [
+            'Azotobacter ',
+            'Clostridium',
+            'Rhizobium ',
+            'Lactobacillus',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'An organism having cytoplasm i.e. DNA and RNA but no cell wall is',
+        options: [
+            'Cyanobacterium ',
+            'Mycoplasma',
+            'Bacterium ',
+            'Virus',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Kingdom monera comprises the',
+        options: [
+            'Plants of economic importance',
+            'All the plants studied in botany',
+            'Prokaryotic organisms',
+            'Plants of Thallophyta group',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The cell wall of green plants is made up of',
+        options: ['Pectin ', 'Suberin', 'Cellulose ', 'Chitin'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following is not a blue-green algae ?',
+        options: [
+            'Nostoc ',
+            'Anabaena',
+            'Lichen ',
+            'Aulosiras',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'During rainy seasons, the ground becomes slippery due to dense growth of',
+        options: [
+            'Lichens ',
+            'Bacteria',
+            'Green algae ',
+            'Cyanobacteria',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Paramecium is a',
+        options: [
+            'Protozoan ',
+            'Bacterium',
+            'Virus ',
+            'Annelid',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Protists are',
+        options: [
+            'single-celled eukaryotes ',
+            'multicellular eukaryotes',
+            'single-celled prokaryotes ',
+            'single-celled akaryote',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Total parasites belong to protozoan group',
+        options: [
+            'Sporozoa ',
+            'Ciliata',
+            'Sarcodina ',
+            'Zooflagellata',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The cilia in Paramecium are',
+        options: [
+            'All equal ',
+            'All unequal',
+            'Longer at posterior end ',
+            'Longer at anterior end',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Plasmodium, the parasite, belongs to class',
+        options: [
+            'Sarocodina ',
+            'Ciliata',
+            'Sporozoa ',
+            'Dinophyceae',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which of the following organisms were never included in protista ?',
+        options: [
+            'Bacteria ',
+            'Red algae',
+            'Slime moulds ',
+            'Mosses',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following does not contain chlorophyll ?',
+        options: [
+            'Fungi ',
+            'Algae',
+            'Bryophyta ',
+            'Pteridophyta',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following statements are true about bacteria?',
+        options: [
+            'They are the sole members of the kingdom Monera.',
+            'They live in extreme habitats such as hot springs, deserts, snow and deep oceans',
+            'They show the most extensive metabolic diversity',
+            'All of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The cell wall of fungi is made up of',
+        options: ['Chitin ', 'Cellulose', 'Pectin ', 'Suberin'],
+        answer: 1,
+    },
+    {
+        question:
+            'The disease of potato responsible for famous famine of Europe was caused by or late blight of potato is caused by',
+        options: [
+            'Colletotrichum falcatum',
+            'Phytophthora infestans',
+            'Potato mosaic virus',
+            'Alternaria solani',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Ergot is caused by',
+        options: [
+            'Claviceps ',
+            'Penicillium',
+            'Aspergillus ',
+            'Rhizobium',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'When fungi feed on dead organic matter, they are known as',
+        options: [
+            'Dimorphic ',
+            'Parasites',
+            'Saprophytes ',
+            'None of these',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which of the following divisions of fungi includes Club fungi ?',
+        options: [
+            'Zygomycota ',
+            'Ascomycota',
+            'Deuteromycota ',
+            'Basidiomycota',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following fungi are edible ?',
+        options: [
+            'Agaricus campestris ',
+            'Morchella esculenta',
+            'Podaxon prodaxis ',
+            'All of these',
+        ],
+        answer: 4,
+    }
+];
