30ad77ca42a89acdd68c3a969720d9936f191d52
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-21 15:39:42 +0530

    Create plant kingdom conceptual2.js

---
+++ plant kingdom conceptual2.js
@@ -0,0 +1,311 @@
+let quiz = [
+    {
+        question:
+            'Which one of the following pairs of plants are not seed producers?',
+        options: [
+            'Funaria and Pinus ',
+            'Fern and Funaria',
+            'Funaria and Ficus ',
+            'Ficus and Chlamydomonas',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Fern plant is a',
+        options: [
+            'Haploid gametophyte',
+            'Diploid gametophyte',
+            'Diploid sporophyte',
+            'Haploid sporophyte',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Seed habit first originated in',
+        options: [
+            'Certain pteridophytes ',
+            'Certain pines',
+            'Certain monocots ',
+            'Certain dicots',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following plants exhibit independent alternation of generation ?',
+        options: [
+            'Angiosperms ',
+            'Gymnosperms',
+            'Pteridophytes ',
+            'Bryophytes',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Laminaria (kelp) and Fucus (rock weed) are the examples of',
+        options: [
+            'red algae ',
+            'brown algae',
+            'green algae ',
+            'golden brown algae',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Protonema occurs in the life cycle of',
+        options: ['Riccia ', 'Funaria', 'Chara ', 'Spirogyra'],
+        answer: 2,
+    },
+    {
+        question: 'Indusium is found in',
+        options: ['Fungi ', 'Moss', 'Algae ', 'Fern'],
+        answer: 4,
+    },
+    {
+        question:
+            'The plant group that produces spores and embryo but lacks vascular tissues and seeds is',
+        options: [
+            'pteridophyta ',
+            'rhodophyta',
+            'bryophyta ',
+            'phaeophyta',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Prothallus means',
+        options: [
+            'Immature gametophyte',
+            'Immature sporophyte',
+            'Immature archegonium',
+            'None of these',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Which of the following is not a pteridophyte?',
+        options: [
+            'Ginkgo ',
+            'Selaginella',
+            'Polypodium ',
+            'Azolla',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Mosses and ferns are found in moist and shady places because both',
+        options: [
+            'Require presence of water for fertilization.',
+            'Do not need sunlight for photosynthesis.',
+            'Depend for their nutrition on micro-organisms which can survive only at low temperature.',
+            'Can not compete with sun-loving plants.',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The gametophyte of fern bears',
+        options: [
+            'True roots ',
+            'Antheridia',
+            'Archegonia ',
+            'Both b and c',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Sporangia and spore bearing leaf in fern is called as',
+        options: [
+            'Ramentum ',
+            'Sorus',
+            'Indusium ',
+            'Sporophyll/Frond',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            "43. Which of the following is known as 'resurrection plant'?",
+        options: [
+            'Selaginella ',
+            'Welwitschia',
+            'Rafflesia ',
+            'Chlorella',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Neck canal cells are absent in archegonia of',
+        options: [
+            'Bryophytes ',
+            'Gymnosperms',
+            'Pteridophytes ',
+            'All of these',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'The plant body of moss (Funaria) is',
+        options: [
+            'completely sporophyte',
+            'completely gametophyte',
+            'predominantly sporophyte with gametophyte',
+            'predominantly gametophyte with sporophyte',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Resin canals and mucilage canal of gymnosperms are example of',
+        options: [
+            'Schizogenous cavity ',
+            'Lysigenous cavity',
+            'Large vacuole ',
+            'Inter cellular cavity',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Endosperm in Gymnosperm is formed',
+        options: [
+            'At the time of fertilization',
+            'Before fertilization',
+            'After fertilization',
+            'Along with the development of embryo',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'In Gymnosperm, endosperm is formed by',
+        options: [
+            'Fusion between a male gamete and two polar nuclei',
+            'Fusion between a male gamete and a polar nuclei',
+            'Fusion between egg and male gamete',
+            'Germination of megaspore',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The endosperm of Gymnosperm is',
+        options: [
+            'Haploid ',
+            'Diploid',
+            'Triploid ',
+            'Tetraploid',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Fruits are not formed in Gymnosperms because of',
+        options: [
+            'Absence of pollination',
+            'Absence of seed',
+            'Absence of fertilization',
+            'Absence of ovary',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Phloem element of Gymnosperm lacks',
+        options: [
+            'Sieve element ',
+            'Companion cell',
+            'Parenchyma ',
+            'Sclerenchyma',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Non-porous wood is the characteristic feature of',
+        options: [
+            'Gymnosperm ',
+            'Angiosperm',
+            'Both a and b ',
+            'None of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Archegonia are absent in the ovules of',
+        options: [
+            'Gnetum ',
+            'Welwestchia',
+            'Ephedra ',
+            'Both a and b',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'In Funaria the haploid structure is',
+        options: [
+            'protonema ',
+            'capsule',
+            'columella ',
+            'seta',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'In which of the following, fertilization is possible without water ?',
+        options: [
+            'Algae ',
+            'Bryophytes',
+            'Pteridophytes ',
+            'Gymnosperm',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Multicellular branched rhizoids and leafy gametophytes are characteristic of',
+        options: [
+            'all bryophytes ',
+            'some bryophytes',
+            'all pteridophytes ',
+            'some pteridophytes',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'In Gymnosperms, the seeds are naked because they lack',
+        options: [
+            'Integuments ',
+            'Pericarp',
+            'Nucellus ',
+            'Parienth',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Cones and flowers are similar because ',
+        options: [
+            'Both are bright and showy',
+            'Both are reproductive organs',
+            'Do not contain endosperm',
+            'Do not contain starch',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Pollination of pollen grains in Pinus takes place at',
+        options: [
+            'One celled stage ',
+            'Two celled stage',
+            'Three celled stage ',
+            'Four celled stage',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following structures in Pinus are haploid?',
+        options: [
+            'Megaspore, integument, root',
+            'Endosperm, megaspore, pollen grain',
+            'Pollen grain, leaf, root',
+            'Megaspore, endosperm, embryo',
+        ],
+        answer: 2,
+    },
+];
