77975cb074cd3924031b1f2e34861dd4e1487ff0
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-21 15:40:29 +0530

    Create plant kingdom conceptual3.js

---
+++ plant kingdom conceptual3.js
@@ -0,0 +1,143 @@
+let quiz = [
+    {
+        question: 'In Pinus male and female reproductive structures occur',
+        options: [
+            'On different branches of the same plant',
+            'On different plants',
+            'On same branch',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Coralloid roots of Cycas are useful in',
+        options: [
+            'N2 -fixation ',
+            'Absorption of water',
+            'Transpiration ',
+            'Fixation',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following is a important characteristic of angiosperms?',
+        options: [
+            'presence of vessels ',
+            'double fertilisation',
+            'secondary growth ',
+            'autotrophic nutrition',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Perisperm is remaining part of',
+        options: [
+            'Endosperm ',
+            'Ovule',
+            'Nucellus ',
+            'Integument',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'If there are 4 cells in anther, what will be the number of pollen grains',
+        options: ['8 ', '4', '16 ', '12'],
+        answer: 3,
+    },
+    {
+        question:
+            'If the cells of root in wheat plant have 42 chromosomes, then the no. of chromosome in the cell of pollen grain is',
+        options: ['14 ', '21', '28 ', '42'],
+        answer: 2,
+    },
+    {
+        question: 'Vessels and companion cells are characteristics of',
+        options: [
+            'Angiosperm ',
+            'Gymnosperm',
+            'Pteridophyta ',
+            'Fern',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Angiosperms and gymnosperms resemble in having',
+        options: [
+            'Vessels in wood ',
+            'Mode of fertilization',
+            'Siphonogamy ',
+            'Nature of endosperm',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'In a moss the sporophyte',
+        options: [
+            'produces gametes that give rise to the gametophyte',
+            'arises from a spore produced from the gametophyte',
+            'manufactures food for itself as well as for the gametophyte',
+            'is partially parasitic on the gametophyte',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Spore dissemination in some liverworts is aided by',
+        options: [
+            'indusium ',
+            'calyptra',
+            'peristome teeth ',
+            'elaters',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'In which one of the following, male and female gametophytes do not have free living independent existence?',
+        options: [
+            'Pteris ',
+            'Funaria',
+            'Polytrichum ',
+            'Cedrus',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Male and female gametophytes are independent and free - living in:',
+        options: ['Mustard ', 'Castor', 'Pinus ', 'Sphagnum'],
+        answer: 4,
+    },
+    {
+        question: 'In angiosperms, functional megaspore develops into',
+        options: [
+            'embryo sac ',
+            'ovule',
+            'endosperm ',
+            'pollen sac',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Cycas and Adiantum resemble each other in having:',
+        options: [
+            'Seeds ',
+            'Motile Sperms',
+            'Cambium ',
+            'Vessels',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Gymnosperms are also called soft wood spermatophytes because they lack :',
+        options: [
+            'Cambium ',
+            'Phloem fibres',
+            'Thick-walled tracheids ',
+            'Xylem fibres',
+        ],
+        answer: 4,
+    },
+];
