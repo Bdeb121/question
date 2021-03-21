9aaec275312e169663f95c3057c1d9246e12efa7
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-21 15:37:40 +0530

    Create plant kingdom conceptual1.js

---
+++ plant kingdom conceptual1.js
@@ -0,0 +1,296 @@
+let quiz = [
+    {
+        question:
+            'The yield of paddy can be increased by the application of',
+        options: [
+            'Nostoc ',
+            'Symbiotic bacteria',
+            'Iron bacteria ',
+            'Archaebacteria',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Red tides are caused by',
+        options: [
+            'Anabaena ',
+            'Nostoc',
+            'Gleocapsa ',
+            'Trichodesmium',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which blue-green alga remain in symbiotic association with Anthoceros ?',
+        options: [
+            'Azolla ',
+            'Spirochaete',
+            'Spirullina ',
+            'Nostoc',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Alga associated with Cycas root is',
+        options: [
+            'Anabaena ',
+            'Chara',
+            'Chlorella ',
+            'Cladophora',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'What is true about male and female gametophyte in plant kingdom ?',
+        options: [
+            'In bryophytes and pteridophytes they have independent free-living existence.',
+            'In gymnosperms and angiosperms they have no independent free-living existence.',
+            'Both a and b',
+            'In bryophytes, pteridophytes and angiosperms they have free-living life. They remain in sporangia which are retained on sporophytes.',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Agar-agar is obtained from',
+        options: [
+            'Gigartina ',
+            'Gelidium',
+            'Gracillaria ',
+            'All of these',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which of the following occurs both in fresh as well as in marine water?',
+        options: [
+            'Oedogonium ',
+            'Cladophora',
+            'Spirogyra ',
+            'None of these',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Which one is a parasitic algae?',
+        options: [
+            'Vaucheria ',
+            'Polysiphonia',
+            'Cephaleuros ',
+            'Batrachospermum',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Which one is incorrect character of brown algae ?',
+        options: [
+            'Presence of chl a and b',
+            'It remain attached to substratum',
+            'Presence of chl a and c',
+            'Presence of fucoxanthin',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Pyrenoids in green algal cells are related to',
+        options: [
+            'Starch formation',
+            'Protein storage',
+            'General metabolism',
+            'Enzyme secretion',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The product of conjugation of Spirogyra is called',
+        options: [
+            'Zoospore ',
+            'Akinete',
+            'Chlamydospore ',
+            'Zygospore',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which type of sexual reproduction is found in Spirogyra?',
+        options: [
+            'Conjugation ',
+            'Binary fission',
+            'Fragmentation ',
+            'Spores formation',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Reindeer moss is the common name of',
+        options: [
+            'Usnea comosa',
+            'Cladonia rangifera',
+            'Funaria hygrometrica',
+            'Sphagnum aceutifolium',
+        ],
+        answer: 2,
+    },
+    {
+        question: "14. Which commonly known as 'Peat moss' or 'Bog moss' ?",
+        options: [
+            'Polytrichum ',
+            'Funaria',
+            'Sphagnum ',
+            'Riccia',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Elaters are present in capsule of',
+        options: [
+            'Riccia ',
+            'Marchantia',
+            'Anthoceros ',
+            'Funaria',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'In Funaria',
+        options: [
+            'Outer peristome teeth are hygroscopic than the inner teeth',
+            'Both the types of teeth are hygroscopic',
+            'Inner teeth are responsible for spores dispersal',
+            'Outer teeth are diploid and inner haploid',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'A bryophyte differs from pteridophytes in',
+        options: [
+            'Archegonia',
+            'Lack of vascular tissue',
+            'Swimming antherozoids',
+            'Independent gametophytes',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'The unique feature of bryophytes compared to other plant groups is that',
+        options: [
+            'They produce spores',
+            'They lack vascular tissues',
+            'They lack roots',
+            'Their sporophyte is attached to the gametophyte',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Chloroplast of Chlamydomonas is',
+        options: [
+            'stellate-shaped ',
+            'cup-shaped',
+            'collar-shaped ',
+            'spiral-shaped',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Sporophyte is dependent on gametophyte in',
+        options: [
+            'Bryophytes ',
+            'Gymnosperms',
+            'Angiosperms ',
+            'Pteridophytes',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Floridean starch is found in',
+        options: [
+            'chlorophyceae ',
+            'rhodophyceae',
+            'myxophyceae ',
+            'cyanophyceae',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Bryophytes are different from fungi in having',
+        options: [
+            'Land habit',
+            'Sterile jacket layers',
+            'Multiflagellate gametes',
+            'Gametophytic plant body',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Funaria may be differentiated from Pinus by the character',
+        options: [
+            'No fruits are produced',
+            'No seeds are produced',
+            'Antheridia and archegonia',
+            'Both a and b',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Female reproductive part of bryophytes is',
+        options: [
+            'Antheridium ',
+            'Oogonium',
+            'Archegonium ',
+            'Sporangium',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'A research student collected certain alga and found that its cells contained both chlorophyll a and chlorophyll b as well as phycoerythrin.The alga belongs to',
+        options: [
+            'rhodophyceae ',
+            'bacillariophyceae',
+            'chlorophyceae ',
+            'phaeophyceae',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Protonema stage is found in',
+        options: ['Ferns ', 'Mosses', 'Liverworts ', 'Fungi'],
+        answer: 2,
+    },
+    {
+        question: 'Apophysis in moss capsule is',
+        options: [
+            'Upper part ',
+            'Middle part',
+            'Lower part ',
+            'Fertile part',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following stage of Funaria is haploid?',
+        options: [
+            'Gametophte ',
+            'Sporophyte',
+            'Both a and b ',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which among the following is considered advanced among pteridophytes ?',
+        options: [
+            'Club mosses ',
+            'Ferns',
+            'Quillworts ',
+            'Scouring rushers',
+        ],
+        answer: 2,
+    },
+];
