7b5ecee82e4fe80f6beaf039f97875f00657e2fc
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-23 15:38:34 +0530

    Create Reproduction in Organisms conceptual1.js

---
+++ Reproduction in Organisms conceptual1.js
@@ -0,0 +1,246 @@
+let quiz = [
+    {
+        question: 'Binary fission is found in',
+        options: [
+            'Amoeba ',
+            'Paramecium',
+            'Planaria ',
+            'All of these',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Basal half of an onion bulb is removed and upper half is sown in the ground. New plant will',
+        options: [
+            'emerge normally ',
+            'not emerge',
+            'be without leaves ',
+            'be without flowers',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'A small portion of cane-sugar stem between the two successive nodes is cut off and sown in the soil normally. New plants will',
+        options: [
+            'be formed normally ',
+            'not be formed',
+            'be without juice ',
+            'without nodes',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'What is necessary for successful grafting to occur ?',
+        options: [
+            'Each section must be able to form roots.',
+            'The grafted section must be able to form seeds.',
+            'Fusion of the two vascular tissues must occur.',
+            'Fusion of the two cambial tissues must occur.',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'In all the methods of asexual reproduction',
+        options: [
+            'offsprings produced are genetically identical to the parents.',
+            'offsprings produced are genetically different from the parents.',
+            'offsprings produced may or may not be identical to the parents.',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Banana fruits are seedless, because',
+        options: [
+            'auxins are sprayed for rapid development of fruit.',
+            'vegetative propagation of plant.',
+            'they are triploid plants.',
+            'fruits are artificially ripened.',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Soil around stem of potato plant is constantly removed from very young stage so that only roots remain in the soil. Potato tubers in plant will',
+        options: [
+            'be larger ',
+            'be smaller',
+            'be formed normally ',
+            'not be formed',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'A potato tuber is sown in a pot and pot is placed in the freezer of refrigerator. New plants will',
+        options: [
+            'emerge normally ',
+            'not emerge',
+            'be without leaves ',
+            'be without flowers',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Asexual reproduction takes place in',
+        options: [
+            'higher animals ',
+            'lower animals',
+            'plants ',
+            'All of these',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'As a result of binary fission number of individuals produced by one fission is',
+        options: ['two ', 'three', 'four ', 'five'],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following definitions about oviparity is false ?',
+        options: [
+            'Only birds and reptiles are oviparous.',
+            'The large amount of yolk provides the nutrients for the developing embryo.',
+            'The shell protects the egg from dehydration.',
+            'Both oxygen and carbon dioxide can diffuse through the shell.',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Seeds are regarded to be the product of sexual reproduction because they',
+        options: [
+            'Can be stored for a long period.',
+            'Give rise to new plants.',
+            'Are the result of fusion of male gamete with the female gamete.',
+            'None of these',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which of the following statements about animals that utilize external fertilization is false?',
+        options: [
+            'They are divided equally between terrestrial and aquatic species.',
+            'Many produce large numbers of gametes to ensure successful reproduction.',
+            'The behaviours associated with mating are often highly synchronized.',
+            'The probability of any one egg being fertilized and developing into an adult can be low.',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Consider the following statements and choose the correct option.',
+        statement: [
+            '(I) The genetic constitution of a plant is unaffected in vegetative propagation.',
+            '(II) Rhizome in ginger serves as an organ of vegetative reproduction.',
+            '(III) Totipotency of cells enables us to micropropagate plants.',
+        ],
+        options: [
+            'Statements (I) and (II) alone are true',
+            'Statements (II) and (III) alone are true',
+            'Statement (II) alone is true',
+            'All the three statements (I), (II) and (III) are true',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which of the following animals qualifies as a sexually reproducing, oviparous species ?',
+        options: ['Human ', 'Chicken', 'Kangaroo ', 'Sea star'],
+        answer: 2,
+    },
+    {
+        question:
+            'During spermatogenesis a single male germ cell produces _____ sperm cell(s).',
+        options: ['1 ', '2', '3 ', '4'],
+        answer: 4,
+    },
+    {
+        question: 'Find the correct combination.',
+        options: [
+            'Zoospore in sponge ',
+            'Gemmules in Penicillium',
+            'Conidia in algae ',
+            'Buds in Hydra',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The chromosome number is haploid in',
+        options: ['zygote ', 'gamete', 'embryo ', 'seed'],
+        answer: 2,
+    },
+    {
+        question: 'Plants identical to mother plants can be obtained from',
+        options: [
+            'seeds ',
+            'stem cutting',
+            'Both and ',
+            'None of these',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'A clone is a group of individuals obtained through',
+        options: [
+            'self-pollination',
+            'hybridization',
+            'vegetative propagation',
+            'cross-pollination',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Ploidy of ovary, anther, egg, pollen, male gamete and zygote are respectively-',
+        options: [
+            '2n, 2n, n, 2n, n, 2n ',
+            '2n, 2n, n, n, n, 2n',
+            '2n, n, n, n, n, n ',
+            '2n, 2n, n, 2n, 2n, 2n',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Fleshy buds produced in the axil of leaves, which grow to form new plants when shed and fall on ground, are called',
+        options: ['Bulbs ', 'Bulbils', 'Tubers ', 'Offsets'],
+        answer: 2,
+    },
+    {
+        question:
+            'Offsprings of oviparous animals are at greater risk as compared to offsprings of viviparous animals because-',
+        options: [
+            'Proper embryonic care and protection is lesser',
+            'Embryo is not developed',
+            'Progenies are with more variation',
+            'Progenies are larger',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Syngamy means',
+        options: [
+            'fusion of similar spores.',
+            'fusion of gametes.',
+            'fusion of dissimilar spores.',
+            'fusion of cytoplasm.',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'A plant differs from an animal mainly in:',
+        options: [
+            'protoplasm ',
+            'vital activities',
+            'nutrition ',
+            'reproduction',
+        ],
+        answer: 3,
+    },
+];
