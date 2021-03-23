e1eee9a8ae9a7911db10c3a0b8c1d19f7804662e
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-23 15:39:19 +0530

    Create Reproduction in Organisms conceptual2.js

---
+++ Reproduction in Organisms conceptual2.js
@@ -0,0 +1,256 @@
+let quiz = [ 
+    {
+        question: 'Oestrus cycle is seen in-',
+        options: [
+            'Cows and sheep ',
+            'Rats and deers',
+            'Dogs and tiger ',
+            'All of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The parameter(s) of senescence or old age is/are-',
+        options: [
+            'End of the reproductive phase',
+            'Cocomitant change in body (like slowing metabolism)',
+            'Failure of metabolism decreases',
+            'Both a and b',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which is the correct sequence?',
+        options: [
+            'Gametogenesis – syngamy – embryogenesis – zygote',
+            'Syngamy – gametogenesis – zygote – embryogenesis',
+            'Gametogenesis – syngamy – zygote – embryogenesis',
+            'Zygote – embryogenesis – syngamy – gametogenesis',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The terms homothallic and monoecious are used to denote',
+        options: [
+            'bisexual condition ',
+            'unisexual condition',
+            'staminate flowers ',
+            'pistillate flowers',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Seed formation without fertilization in flowering plants involves the process of :',
+        options: [
+            'Sporulation',
+            'Budding',
+            'Somatic hybridization',
+            'Apomixis',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'What is true for cleavage?',
+        options: [
+            'Size of embryo increases',
+            'Size of cells decrease',
+            'Size of cells increase',
+            'Size of embryo decreases',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'During regeneration, modification of an organ to other organ is known as',
+        options: [
+            'Morphogenesis ',
+            'Epimorphosis',
+            'Morphallaxis ',
+            'Accretionary growth',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Blastopore is the opening of',
+        options: [
+            'coelenteron ',
+            'coelom',
+            'blastocoel ',
+            'archenteron',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The correct sequence in the process of development of human embryo is',
+        options: [
+            'fertilization—zygote—cleavage—morula—blastula—gastrula',
+            'fertilization—cleavage—morula—zygote—blastula—gastrula',
+            'fertilization—zygote—blastula—morula—cleavage—gastrula',
+            'cleavage—zygote—fertilization—morula—blastula—gastrula',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'What is true about cleavage in fertilized egg of humans?',
+        options: [
+            'Meroblastic',
+            'Starts when egg reaches uterus',
+            'Starts in fallopian tube',
+            'It is identical to normal mitosis',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Termination of gastrulation is indicated by',
+        options: [
+            'obliteration of blastocoel',
+            'obliteration of archenteron',
+            'closure of blastopore',
+            'closure of neural tube',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Meroblastic cleavage is division',
+        options: [
+            'horizontal ',
+            'partial/parietal',
+            'total ',
+            'spiral',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'During cleavage, what is true about cells?',
+        options: [
+            'Nucleocytoplasmic ratio remains unchanged',
+            'Size does not increase',
+            'There is less consumption of oxygen',
+            'The division is like meiosis',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Cells become variable in morphology and function in different regions of the embryo. The process is',
+        options: [
+            'differentiation ',
+            'metamorphosis',
+            'organisation ',
+            'rearrangement',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Plants are like animals in that',
+        options: [
+            'both respire in the day only',
+            'both respire in the night only',
+            'both respire day and night',
+            'both respire when required',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Earthworms, sponges, tapeworms are',
+        options: [
+            'bisexual animals ',
+            'unisexual animals',
+            'hermaphrodites ',
+            'Both and (c)',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Asexual reproduction is called as',
+        options: [
+            'apomixis ',
+            'fragmentation',
+            'self-fertilization ',
+            'cross-fertilization',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which of the following is cultivated through vegetative propagation?',
+        options: [
+            'Potato and sugarcane ',
+            'Banana and ginger',
+            'Dahlia and rose ',
+            'All of these',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The site of origin of the new plantlets in potato, dahlia, ginger and banana is-',
+        options: [
+            'Floral buds present on stem',
+            'Internodes of modified stem',
+            'Nodes of modified stem',
+            'Adventitious buds present on root',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Among the following which one is not a method of vegetative propagation?',
+        options: [
+            'Budding ',
+            'Layering',
+            'Sowing ',
+            'Tissue culture',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The “Eyes” of the potato tuber are:',
+        options: [
+            'root buds ',
+            'flower buds',
+            'shoot buds ',
+            'axillary buds',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The formation of gametophyte from the sporophyte (without meiosis) is called',
+        options: [
+            'apospory ',
+            'apogamy',
+            'agametospermy ',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Internal fertilization occurs in-',
+        options: [
+            'Many fungi.',
+            'Reptiles and mammals.',
+            'Bryophytes, pteridophytes, gymnosperms and angiosperms.',
+            'All of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Corm is:',
+        options: [
+            'swollen horizontal underground stem.',
+            'compressed swollen vertically underground stem.',
+            'swollen end of an underground stem.',
+            'underground bud consisting of fleshy swollen storage food.',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Rhizome which grows vertically upward is',
+        options: ['corm ', 'stolon', 'bulbil ', 'rootstock'],
+        answer: 4,
+    },
+];
