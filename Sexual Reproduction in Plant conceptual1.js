78844eca8b440ede50ef23e9e69c1a5323299969
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-23 15:40:19 +0530

    Create Sexual Reproduction in Plant conceptual1.js

---
+++ Sexual Reproduction in Plant conceptual1.js
@@ -0,0 +1,248 @@
+let quiz = [
+    {
+        question:
+            'How many pollen grains will be formed after meiotic division in ten microspore mother cells?',
+        options: ['10 ', '20', '40 ', '80'],
+        answer: 3,
+    },
+    {
+        question:
+            'Flowers showing ornithophily show few characteristic like',
+        options: [
+            'blue flower with nectaries at base of corolla',
+            'red sweet scented flower with nectaries',
+            'bright red flower into thick inflorescence',
+            'white flowers with fragrance',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which type of association is found in between entomophilous flower and pollinating agent ?',
+        options: [
+            'Mutualism',
+            'Commensalism',
+            'Cooperation',
+            'Co-evolution',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which is the most logical sequence with reference to the life cycle of angiosperm ?',
+        options: [
+            'Germination, endosperm formation, seed dispersal, double fertilization',
+            'Cleavage, fertilization, grafting, fruit formation',
+            'Pollination, fertilization, seed formation & germination',
+            'Maturation, mitosis, differentiation',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Tapetum is',
+        options: [
+            'parietal in origin usually the inner most layer of anther wall.',
+            'modified endothecium of anther wall.',
+            'modified outer most layer of sporogenous tissue.',
+            'parietal in origin and is the inner most layer of ovule wall.',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Which of the following pair has haploid structures?',
+        options: [
+            'Nucellus and antipodal cells',
+            'Antipodal cells and egg cell',
+            'Antipodal cells and megaspore mother cell',
+            'Nucellus and primary endosperm nucleus',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Male gametophyte of angiosperms/monocots is',
+        options: [
+            'Microsporangium ',
+            'Nucellus',
+            'Microspore ',
+            'Stamen',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'The type of cells under going meiosis in the flowers are',
+        options: [
+            'micro spore mother cell & mega spore mother cell',
+            'ovule & stamen',
+            'tapetal cells',
+            'placental cell',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which ones produces androgenic haploids in anther cultures?',
+        options: [
+            'Anther wall',
+            'Tapetal layer of anther wall',
+            'Connective tissue',
+            'Young pollen grains',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Generative cell was destroyed by laser but a normal pollen tube was still formed because',
+        options: [
+            'vegetative cell is not damaged',
+            'contents of killed generative cell stimulate pollen growth',
+            'laser beam stimulates growth of pollen tube',
+            'the region of emergence of pollen tube is not harmed',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The embryo is carefully taken out of pea seed and sown in the soil and watered normally. New plant will',
+        options: [
+            'be healthier ',
+            'be weaker',
+            'not be formed ',
+            'be formed normally',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Pea seeds are sown in a pot. After emergence of cotyledons, the pot is placed in an aerated dark chamber and watered normally, seedlings will',
+        options: [
+            'form normal plants',
+            'form healthier plants',
+            'die',
+            'grow very rapidly without forming leaves',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'If there are 4 cells in anther, what will be the number of pollen grains ?',
+        options: ['8 ', '4', '16 ', '12'],
+        answer: 3,
+    },
+    {
+        question:
+            'A cutting of rose plant is thoroughly waxed and planted in the soil normally, it will form',
+        options: [
+            'new rose plant.',
+            'a dead piece of rose stem after some time.',
+            'a rose plant of improved variety.',
+            'None of these',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'In angiospermic plant having chromosome number of 12 will have chromosome number in integuments and nucellus of',
+        options: ['4 ', '6', '12 ', '24'],
+        answer: 3,
+    },
+    {
+        question: 'What is the fate of the seven cells of the embryo sac ?',
+        options: [
+            'All but one disintegrate upon fertilization.',
+            'Two become fertilized; the others disintegrate.',
+            'Two become fertilized; the others fuse to form endosperm.',
+            'All are involved in nuclear fusion events.',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'The most common type of pollination is –',
+        options: [
+            'entemophily',
+            'ornithophily',
+            'malacophily (by snails)',
+            'chiropterophily (by bats)',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'When an ovary develops into a fruit, without fertilization, it is called',
+        options: [
+            'apospory ',
+            'apogamy',
+            'parthenocarpy ',
+            'porogamy',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following has largest gametophyte ?',
+        options: ['Oryza ', 'Funaria', 'Pinus ', 'Selaginella'],
+        answer: 2,
+    },
+    {
+        question: 'Exine of pollen is formed by activity of',
+        options: [
+            'tapetum ',
+            'endothelium',
+            'middle layers ',
+            'endothecium',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following statements about pollen and pollination is false ?',
+        options: [
+            'Evolution of the pollen grain rejected the need for swimming sperm in flowering plants.',
+            'At maturity, the pollen grain consists of two sperm nuclei and a tube nucleus.',
+            'The pollen tube enters the female gametophyte through the style.',
+            'The pollen grain makes twice the genetic contribution to endosperm cells than it does to the cells of the embryo.',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following options is correct?',
+        options: [
+            'Pollination gives the guarantee of the promotion of postpollination events that lead to fertilization.',
+            'The events – “from pollen deposition on stigma until pollen tubes enter the ovule” are together referred to as pollen-pistil interaction.',
+            'Pollen-pistil interaction is a dynamic process involving pollen recognition followed by only promotion (not rejection) of the pollen.',
+            'Pistil has no ability to recognise the pollen, whether right or wrong type.',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Cross-pollination is preferred over self-pollination because',
+        options: [
+            'it results in better offspring.',
+            'the new varieties are formed.',
+            'it is easy.',
+            'parthenogenesis can be induced.',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Seminiferous plant is',
+        options: [
+            'having only staminate flowers.',
+            'reproducing by seeds.',
+            'reproducing by vegetative propagation.',
+            'None of the above',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Which is the correct order of events for female gametophytes?',
+        options: [
+            'Megagametophyte; megasporocyte; megaspore',
+            'Megagametophyte; megaspore; megasporocyte',
+            'Megasporocyte; megaspore; megagametophyte',
+            'Megaspore; megasporocyte; megagametophyte',
+        ],
+        answer: 3,
+    },
+];
