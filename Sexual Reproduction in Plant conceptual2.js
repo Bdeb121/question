fa1d835d23be4606ab505bed947ee48b88328f9d
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2021-03-23 15:41:15 +0530

    Create Sexual Reproduction in Plant conceptual2.js

---
+++ Sexual Reproduction in Plant conceptual2.js
@@ -0,0 +1,260 @@
+let quiz = [
+    {
+        question: 'Emasculation is not required when flowers are',
+        options: [
+            'bisexual',
+            'intersexual',
+            'unisexual',
+            'either a or b',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Pollen grain (microspore) formation in Cyperus (Cypraceae) is',
+        options: [
+            'very common.',
+            'artificially induced by removal of anthers.',
+            'similar to megasporogenesis in majority of angiosperms.',
+            'similar to the microsporogenesis commonly present within angiosperms.',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Male gametes or sperms are developed from generative cell by',
+        options: [
+            'meiotic division ',
+            'mitotic division',
+            'amitotic division ',
+            'None of these',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'From megasporocyte to egg cell, what processes are required ?',
+        options: [
+            'Meiosis followed by mitosis',
+            'Mitosis followed by meiosis',
+            'Several meiotic divisions only',
+            'Several mitotic divisions only',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The study of formation, growth and development of new individual from an egg is called',
+        options: [
+            'embryology ',
+            'embryogenesis',
+            'morphogenesis ',
+            'embryolysis',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Double fertilisation leading to initiation of endosperm in angiosperms require',
+        options: [
+            'fusion of one polar nucleus and the second male gamete only',
+            'fusion of two polar nuclei and the second male gamete',
+            'fusion of four or more polar nuclei and the second male gamete only',
+            'all the above kinds of fusion in different angiosperms',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'In angiosperms pollen tubes liberate their male gametes into the',
+        options: [
+            'central cell ',
+            'antipodal cell',
+            'egg cell ',
+            'synergids',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Microspore in angiosperms contain',
+        options: [
+            'two prothallial cells ',
+            'one parietal cell',
+            '3 parietal cells ',
+            'None of these',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The cause of dormancy for sometime in fertilized ovule is',
+        options: [
+            'presence of hormone, auxin.',
+            'zygote divides only after certain amount of endosperm is formed.',
+            'presence of growth inhibitor ABA.',
+            'presence of least amount of water.',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'As a pollen tube grows into the female organ, the nucleus that enters the synergid first is called the',
+        options: [
+            'sperm nucleus ',
+            'generative nucleus',
+            'tube nucleus ',
+            'pollen nucleus',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The advantage of cross-fertilization in plants is',
+        options: [
+            'increased genetic recombination.',
+            'that meiosis can occur.',
+            'greater efficiency of pollination.',
+            'that no flowering is needed.',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Middle layers of the microsporangial wall',
+        options: [
+            'shrivel at maturity of anther',
+            'persist but remain thin-walled',
+            'degenerate before maturity',
+            'persist and become thickened',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Pollen grain is liberated in',
+        options: [
+            'one celled stage ',
+            'two celled stage',
+            'three celled stage ',
+            'two or three celled stage',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'If an angiospermic male plant is diploid and female plant tetraploid, the ploidy level of endosperm will be',
+        options: [
+            'haploid ',
+            'triploid',
+            'tetraploid ',
+            'pentaploid',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Fruit-eating bats tend to feed extremely rapidly on fruits and have relatively inefficient digestion (some times defecating seeds as early as an hour after feeding on them). Why are they good seed dispersal agents ?',
+        options: [
+            'Seed survival in bat’s guts is low.',
+            'Undigested seeds are deposited in a heap at the bat’s root site.',
+            'Undigested seeds are deposited near the same plant that produced them.',
+            'Digested seeds are dispersed in bat waste products.',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'In some dicots, no distinct endosperm can be seen. Why ?',
+        options: [
+            'The embryo has digested the endosperm.',
+            'The cotyledons have absorbed the endosperm.',
+            'The seeds never produced endosperm.',
+            'The endosperm has become the seed coat',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Ovules are attached to a parenchymatous cushion called',
+        options: [
+            'nucellus ',
+            'obturator',
+            'conducting tissue ',
+            'placenta',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Milky water of green coconut is',
+        options: [
+            'liquid chalaza ',
+            'liquid nucellus',
+            'liquid endosperm ',
+            'liquid female gametophyte',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Scutellum is present in the embryo of',
+        options: [
+            'Pea ',
+            'Ranunculus',
+            'Triticum ',
+            'None of these',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The suspensor',
+        options: [
+            'gives rise to the embryo.',
+            'is heart-shaped in dicots.',
+            'separates the two cotyledons of dicots.',
+            'ceases to elongate early in embryo development.',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The point at which funiculus touches the ovule is',
+        options: ['chalaza ', 'hilum', 'raphe ', 'endothelium'],
+        answer: 2,
+    },
+    {
+        question: 'Adventive polyembryony in citrus is due to',
+        options: [
+            'nucellus ',
+            'integuments',
+            'zygotic embryo ',
+            'fertilised egg',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Formation of gametophyte directly from sporophyte without meiosis is',
+        options: [
+            'Apospory ',
+            'Apogamy',
+            'Parthenogenesis ',
+            'Amphimixis',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The seed in which endosperm is used by embryo is',
+        options: [
+            'single ',
+            'albuminous',
+            'endospermic ',
+            'non-endospermic',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Formation of diploid embryo sac from diploid vegetative structure is called',
+        options: [
+            'diplospory',
+            'apospory',
+            'adventive polyembryony',
+            'apomixis',
+        ],
+        answer: 2,
+    },
+];
