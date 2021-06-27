b3b2d05f15d8cec6a8c9d14b46b65214b0f4436c
Debarghya Banerjee <63992699+Bdeb121@users.noreply.github.com>
2021-06-28 02:43:08 +0530

    Create Body Fluids.js

---
+++ Body Fluids.js
@@ -0,0 +1,443 @@
+let quiz = [
+    {
+        question:
+            '1. The cardiac pacemaker in a patient fails to function normally. The doctors find that an artificial pacemaker is to be grafted in him. It is likely that it will be grafted at the site of',
+        options: [
+            'Atrioventricular bundle ',
+            'Purkinje system',
+            'Sinuatrial node ',
+            'Atrioventricular node',
+        ],
+        answer: 3,
+    },
+    {
+        question: '2. Child death may occur in the marriage between',
+        options: [
+            'Rh+ man and Rh+ woman',
+            'Rh+ man and Rh– woman',
+            'Rh– man and Rh– woman',
+            'Rh– man and Rh+ woman',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            '3. An artificial pacemaker is implanted subcutaneously and connected to the heart in patients',
+        options: [
+            'Having 90% blockage of the three main coronary arteries',
+            'Having a very high blood pressure',
+            'With irregularity in the heart rhythm',
+            'Suffering from arteriosclerosis',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            '4. Blocking of arteries due to deposition of fats and calcium is called',
+        options: [
+            'Arteriosclerosis ',
+            'Atherosclerosis',
+            'Emphysema ',
+            'Heart syndrome',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            '5. Which one of the following doctors performed the first heart transplant ?',
+        options: [
+            'Hargovind Khurana ',
+            'Christian Barnard',
+            'Watson ',
+            'William Harvey',
+        ],
+        answer: 2,
+    },
+    {
+        question: '6. QRST is related with',
+        options: [
+            'Ventricular contraction or depolarization',
+            'Auricular contraction',
+            'Auricular relaxation',
+            'Cardiac cycle',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            '7. Persons suffering from high blood pressure should take the following precaution to avoid excessive rise in their blood pressure',
+        options: [
+            'sleep as much as possible',
+            'avoid standing',
+            'increase their weight',
+            'avoid emotional disturbances and excitement',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            '8. You are required to draw blood from a patient and to keep the blood in a test tube for analysis of corpuscles and plasma. You are also provided with the following four types of test tubes. Which of them will you not use for the purpose?',
+        options: [
+            'Test tube containing calcium bicarbonate',
+            'Chilled test-tube',
+            'Test-tube containing heparin',
+            'Test-tube containing sodium oxalate',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            '9. In the ABO system of blood groups, if both antigens are present but no antibody, the blood group of the individual would be',
+        options: ['B ', 'O', 'AB ', 'A'],
+        answer: 3,
+    },
+    {
+        question: '10. Which of the following statments is true for lymph?',
+        options: [
+            'WBC and serum',
+            'All components of blood except RBCs and some proteins',
+            'RBCs, WBCs and plasma',
+            'RBCs proteins and platelets',
+        ],
+        answer: 2,
+    },
+    {
+        question: '11. What is true about leucocytes ?',
+        options: [
+            'Their sudden fall in number is indication of blood cancer',
+            'These are produced in thymus',
+            'These are enucleated',
+            'These can squeeze out through the capillary walls',
+        ],
+        answer: 4,
+    },
+    {
+        question: '12. Pulmonary artery differ from pulmonary vein in having',
+        options: [
+            'no endothelium ',
+            'valves',
+            'large lumen ',
+            'thick muscular walls',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            '13. Which one of the following statements about blood constituents and transport of respiratory gases is most accurate ?',
+        options: [
+            'RBCs transport oxygen whereas WBCs transport CO2',
+            'RBCs transport oxygen whereas plasma transports only CO2',
+            'RBCs as well as WBCs transport both oxygen and CO2',
+            'RBCs as well as plasma transport both oxygen and CO2',
+        ],
+        answer: 4,
+    },
+    {
+        question: '14. The lymph serves to',
+        options: [
+            'transport oxygen to the brain',
+            'transport carbon dioxide to the lungs',
+            'return the interstitial fluid to the blood',
+            'return the WBCs and RBCs to the lymph nodes',
+        ],
+        answer: 3,
+    },
+    {
+        question: '15. Carbonic anhydrase occurs in',
+        options: ['Lymphocytes ', 'Blood plasma', 'RBC ', 'Leucocytes'],
+        answer: 3,
+    },
+    {
+        question:
+            '16. Fastest distribution of some injectible material/medicine and with no risk of any kind can be achieved by injecting it into the',
+        options: ['Muscles ', 'Arteries', 'Veins ', 'Lymph vessels'],
+        answer: 3,
+    },
+    {
+        question:
+            '17. Given below are four statements (a-d) regarding human blood circulatory system, Which two of the below statements are correct?',
+        statement: [
+            '(1) Arteries are thick-walled and have narrow lumen as compared to veins',
+            '(2) Angina is acute chest pain when the blood circulation to the brain is reduced',
+            '(3) Persons with blood group AB can donate blood to any person with any blood group under ABO system',
+            '(4) Calcium ions play a very important role in blood clotting',
+        ],
+        options: [
+            '(a) (1) and (4)',
+            '(b) (1) and (2)',
+            '(c) (2) and (3) ',
+            '(d) (3) and (4)',
+        ],
+        answer: 1,
+    },
+    {
+        question: '18. Which one engulfs pathogens rapidly?',
+        options: ['Acidophils ', 'Monocytes', 'Basophils ', 'Neutrophils'],
+        answer: 4,
+    },
+    {
+        question: '19. Breakdown product of haemoglobin is',
+        options: ['Bilirubin ', 'Iron', 'Biliverdin ', 'Calcium'],
+        answer: 2,
+    },
+    {
+        question: '20. Bundle of His is a network of',
+        options: [
+            'nerve fibres found throughout the heart',
+            'muscle fibres distributed throughout the heart walls',
+            'muscle fibres found only in the ventricle wall',
+            'nerve fibres distributed in ventricles',
+        ],
+        answer: 3,
+    },
+    {
+        question: '21. The pacemaker of the human heart is',
+        options: ['SA node ', 'tricuspid valve', 'AV node ', 'SV node'],
+        answer: 1,
+    },
+    {
+        question: '22. Pulmonary artery carries',
+        options: [
+            'deoxygenated blood from heart to lungs',
+            'deoxygenated blood from lungs to heart',
+            'oxygenated blood from heart to lungs',
+            'oxygenated blood from lungs to heart',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            '23. The correct route through which pulse-making impulse travels in the heart is',
+        options: [
+            'AV node ® bundle of His ® SA node Purkinje fibres ® heart muscles',
+            'AV node ® SA node ® Purkinje fibres ® bundle of His ® heart muscles',
+            'SA node ® Purkinje fibre ® bundle of His ® AV node ® heart muscles',
+            'SA node ® AV node ® bundle of His ® Purkinje fibre ® heart muscles',
+        ],
+        answer: 4,
+    },
+    {
+        question: '24. A four chambered heart is not found in',
+        options: ['Mammals ', 'Birds', 'Snake ', 'Crocodile'],
+        answer: 3,
+    },
+    {
+        question:
+            '25. Which of the following cations is required for the conversion of prothrombin into active thrombin by thromboplastin?',
+        options: ['Cu2+ ', 'Fe3+', 'Fe2+ ', 'Ca2+'],
+        answer: 4,
+    },
+    {
+        question: '26. The blood protein involved in blood coagulation is',
+        options: ['Heparin ', 'Prothrombin', 'Thrombin ', 'All of these'],
+        answer: 2,
+    },
+    {
+        question: '27. Neurogenic heart is characteristic of',
+        options: ['Lower vertebrates ', 'Humans', 'Rat ', 'Rabbit'],
+        answer: 1,
+    },
+    {
+        question: '28. Heart beats are accelerated by',
+        options: [
+            'Cranial nerves and acetylcholine',
+            'Sympathetic nerves and acetylcholine',
+            'Cranial nerves and adrenaline',
+            'Sympathetic nerves and epinephrine',
+        ],
+        answer: 4,
+    },
+    {
+        question: '29. When the heart beat increases the condition is called',
+        options: [
+            'Bradycardia ',
+            'Tachy cardia',
+            'Leucopenia ',
+            'Cardiac arrest',
+        ],
+        answer: 2,
+    },
+    {
+        question: '30. Purkinje fibres are found in',
+        options: [
+            'Brain',
+            'Skin',
+            'Conduction system of heart',
+            'Nephrons of kidney',
+        ],
+        answer: 3,
+    },
+    {
+        question: '31. Cardiac cycle in man takes about',
+        options: ['0.5 seconds ', '1.0 second', '1.2 seconds ', '0.8 seconds'],
+        answer: 4,
+    },
+    {
+        question:
+            '32. In the evolution of animals a heart to pump the blood is found for the first time in',
+        options: ['Annelids ', 'Roundworms', 'Arthropods ', 'Flat worms'],
+        answer: 1,
+    },
+    {
+        question: '33. In ECG, what does ‘T’ wave represent?',
+        options: [
+            'diastole of auricles',
+            'diastole of ventricles',
+            'systole of ventricles',
+            'diastole of auricles and ventricles',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            '34. The structure of which of the following consist of a layer of single cell thickness',
+        options: ['Blood capillary ', 'Artery', 'Venule ', 'Vessels'],
+        answer: 1,
+    },
+    {
+        question:
+            '35. In man, the normal blood pressure is maintained by the blood protein called',
+        options: ['Haemoglobin ', 'Albumin', 'Fibrinogen ', 'Heparin'],
+        answer: 2,
+    },
+    {
+        question: '36. Chordae tendinae are found in',
+        options: [
+            'Joints of legs ',
+            'atria of heart',
+            'ventricles of brain ',
+            'ventricles of heart',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            '37. Blood pressure increases and heart rate decreases in response to',
+        options: [
+            'Exercise',
+            'Haemorrage',
+            'Exposure to high altitude',
+            'Increased intracranial pressure',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            '38. Systolic pressure of heart is higher than diastolic pressure, because',
+        options: [
+            'blood is forcefully pumped into arteries by the heart during systole and not during diastole',
+            'arteries offer resistance to the flowing of blood during systole only',
+            'arteries contract during systole only.',
+            'volume of blood in heart is greater during systole than during diastole.',
+        ],
+        answer: 1,
+    },
+    {
+        question: '39. Which organ receives only oxygenated blood',
+        options: ['Liver ', 'Pancreas', 'Kidney ', 'Gills'],
+        answer: 2,
+    },
+    {
+        question: '40. In adult, normal blood pressure is–',
+        options: [
+            '80/120 mm Hg ',
+            '100/80 mm Hg',
+            '120/80 mm Hg ',
+            '100/120 mm Hg',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            '41. Bulk of carbon dioxide (CO2) released from body tissues into the blood is present as',
+        options: [
+            'bicarbonate in blood plasma and RBCs',
+            'free CO2 in blood plasma',
+            '70% carbamino- haemoglobin and 30% as bicarbonate',
+            'carbamino-haemoglobin in RBCs',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            '42. Given below is the ECG of a normal human. Which one of its components in human is correctly interpreted below',
+        options: [
+            'Complex QRS-One complete Pulse',
+            'Peak T - Initiation of total cardiac contraction',
+            'Peak P and Peak R together-Systolic and diastolic blood pressures',
+            'Peak P- Initiation of left atrial contraction only',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            '43. Which one of the following statements is correct regarding blood pressure ?',
+        options: [
+            '130/90 mmHg is considered high and requires treatment',
+            '100/55 mmHg is considered an ideal blood pressure',
+            '105/50 mm Hg makes one very active',
+            '90/110 mmHg may harm vital organs like brain and kidney',
+        ],
+        answer: 4,
+    },
+    {
+        question: '44. Arteries are best defined as the vessels which :',
+        options: [
+            'supply oxygenated blood to the different organs',
+            'break up into capillaries which reunite to form one visceral organ',
+            'break up into capillaries which reunite to form a vein',
+            'carry blood from one visceral organ to another visceral organ',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            '45. Which one of the following plasma proteins is involved in the coagulation of blood ?',
+        options: ['an albumin ', 'serum amylase', 'a globulin ', 'fibrinogen'],
+        answer: 4,
+    },
+    {
+        question:
+            '46. ‘Bundle of His’ is a part of which one of the following organs in humans?',
+        options: ['Brain ', 'Heart', 'Kidney ', 'Pancreas'],
+        answer: 2,
+    },
+    {
+        question: '47. The most active phagocytic white blood cells are:',
+        options: [
+            'neutrophils and eosinophils',
+            'lymphocytes and macrophages',
+            'eosinophils and lymphocytes',
+            'neutrophils and monocytes',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            '48. Which type of white blood cells are concerned with the release of histamine and the natural anti- coagulant heparin?',
+        options: ['Neutrophils ', 'Basophils', 'Eosinophils ', 'Monocytes'],
+        answer: 2,
+    },
+    {
+        question:
+            '49. A drop of each of the following, is placed separately on four slides. Which of them will not coagulate?',
+        options: [
+            'Blood serum',
+            'Sample from the thoracic duct of lymphatic system',
+            'Whole blood from pulmonary vein',
+            'Blood plasma',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            '50. Examination of blood of a person suspected of having anemia, shows large, immature, nucleated erythrocytes without haemoglobin. Supple menting his diet with which of the following, is likely to alleviate his symptoms?',
+        options: [
+            'Folic acid and cobalamine ',
+            'Riboflavin',
+            'Iron compounds ',
+            'Thiamine',
+        ],
+        answer: 1,
+    },
+];
