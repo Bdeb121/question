38ddea1774eaca01f8317f5d2d19dff2b996f2bd
Debarghya Banerjee <63992699+Bdeb121@users.noreply.github.com>
2021-06-28 02:43:34 +0530

    Create Breathing1.js

---
+++ Breathing1.js
@@ -0,0 +1,460 @@
+let quiz = [
+    {
+        question:
+            'Which one of the following organs in the human body is most affected due to shortage of oxygen?',
+        options: ['Intestine ', 'Skin', 'Kidney ', 'Brain'],
+        answer: 4,
+    },
+    {
+        question:
+            'What is the cause for the movement of oxygen through the alveolar blood capillaries of lungs ?',
+        options: [
+            'Difference in the O2 tension and partial pressure of these chambers',
+            'Partial pressure of CO2',
+            'Union of O2 with haemoglobin',
+            'All of the above',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Functional residual capacity can be represented as',
+        options: ['TV + ERV ', 'ERV + RV', 'RV + IRV ', 'ERV + TV + IRV'],
+        answer: 2,
+    },
+    {
+        question:
+            'Lungs in addition to the respiratory function also help in –',
+        options: [
+            'Excretion',
+            'Temperature regulation',
+            'pH regulation',
+            'Maintaining balance of body',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'If the thoracic wall but not lungs is punctured-',
+        options: [
+            'The lungs get inflated',
+            'The man dies as the lungs get collapsed',
+            'The breathing rate decreases',
+            'The breathing rate increases',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'During transportation of CO2 when bicarbonate ions diffuses from RBC into plasma, the increased hydrogen ion concentration in RBC is balanced by the entry of which substance from plasma',
+        options: ['Water ', 'Oxygen', 'Hydroxyl ions ', 'Chloride ions'],
+        answer: 4,
+    },
+    {
+        question: 'Common feature of human and insect trachea is –',
+        options: [
+            'Non-collapsible wall ',
+            'Supporting rings',
+            'Ectodermal origin ',
+            'Endodermal origin',
+        ],
+        answer: 1,
+    },
+    {
+        question: '6000 to 8000 ml of air is the',
+        options: [
+            'Vital capacity of lungs',
+            'Volume of normal expiration per minute',
+            'Sum of IRV + ERV',
+            'Inspiratory capacity of lungs',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Which one of the following can respire in the absence of oxygen ?',
+        options: ['Amoeba ', 'Tapeworm', 'House fly ', 'Hydra'],
+        answer: 2,
+    },
+    {
+        question:
+            'The process of migration of chloride ions from plasma to RBC and carbonate ions from RBC to plasma is',
+        options: [
+            'chloride shift ',
+            'ionic shift',
+            'atomic shift ',
+            'Na+ pump',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'In alveoli of the lungs, the air at the site of gas exchange, is separated from the blood by',
+        options: [
+            'alveolar epithelium only',
+            'alveolar epithelium and capillary endothelium',
+            'alveolar epithelium, capillary endothelium and tunica adventitia',
+            'alveolar epithelium, capillary endothelium, a thin layer of tunica media and tunica adventitia',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'The quantity 1500 ml in the respiratory volumes of a normal human adult refers to',
+        options: [
+            'maximum air that can be breathed in and breathed out',
+            'residual volume',
+            'expiratory reserve volume',
+            'total lung capacity',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'The volume of air that remains in the lungs after normal expiration is',
+        options: [
+            'Residual volume',
+            'Vital capacity',
+            'Expiratory capacity',
+            'Functional residual capacity',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The carbon dioxide is transported via blood to lungs as',
+        options: [
+            'dissolved in blood plasma',
+            'in the form of carbonic acid only',
+            'in combination with haemoglobin only',
+            'carbaminohaemoglobin and as carbonic acid',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Although much CO2 is carried in blood, yet blood does not become acidic, because',
+        options: [
+            'it is absorbed by the leucocytes',
+            'blood buffers play an important role in CO2 transport.',
+            'it combines with water to form H2CO3which is neutralized by NaCO3',
+            'it is continuously diffused through tissues and is not allowed to accumulate',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Air is breathed through',
+        options: [
+            'Trachea — lungs — larynx — pharynx — alveoli',
+            'Nose — larynx — pharynx — bronchus — alveoli — bronchioles',
+            'Nostrils — pharynx — larynx — trachea — bronchi — bronchioles — alveoli',
+            'Nose — mouth — lungs',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Carbon dioxide is transported from tissues to respiratory surface by',
+        options: [
+            'plasma and erythrocytes',
+            'plasma',
+            'erythrocytes',
+            'erythrocytes and leucocytes',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following breathing process in human is passive?',
+        options: [
+            'Expiration ',
+            'Aspiration',
+            'Inspiration ',
+            'Forced breathing',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Oxygenated blood from lungs is carried to the heart by –',
+        options: [
+            'Pulmonary artery ',
+            'Pulmonary vein',
+            'Coronary vein ',
+            'Pre-cavals',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Which is correct ?',
+        options: [
+            'Respiratory centres are not affected by CO2',
+            'In humans vital capacity is just double the expiratory volume',
+            'A human lung has 103 alveoli',
+            'During inspiration the lungs act as suction pump',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Body tissues obtain oxygen from haemoglobin because of its dissociation in tissues caused by –',
+        options: [
+            'Low oxygen concentration and high carbon dioxide concentration',
+            'Low oxygen concentration',
+            'Low carbon dioxide concentration',
+            'High carbon dioxide concentration',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'When the carbon dioxide concentration in blood increases, the rate of breathing will –',
+        options: ['remain unaffected ', 'decrease', 'stop ', 'increase'],
+        answer: 4,
+    },
+    {
+        question: 'During inspiration –',
+        options: [
+            'The diaphragm gets raised and ribs get lowered',
+            'Both diaphragm and ribs get raised',
+            'Both diaphragm and ribs get lowered',
+            'The diaphragm get flattered and ribs get raised',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'A normal man at rest inspires and expires about 500 millilitres of air, this amount is known as –',
+        options: [
+            'complemental volume of air',
+            'tidal volume of air',
+            'reserve volume of air',
+            'residual volume of air',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Every 100 ml of oxygenated blood delivers following amount of O2 to the tissues under normal physiological contition',
+        options: ['5 ml ', '25 ml', '50 ml ', 'More the 50 ml'],
+        answer: 1,
+    },
+    {
+        question:
+            'If O2 concentration in tissues were almost as high as at the respiratory surface',
+        options: [
+            'oxyhaemoglobin would dissociate to supply O2 to the tissues',
+            'haemoglobin would combine with more O2 to the tissues',
+            'oxyhaemoglobin would not dissociate to supply O2 to the tissues',
+            'CO2 will interfere with O2 transport.',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'The blood leaving the lungs has all its haemoglobin oxygenated and gives up oxygen to the tissues, because',
+        options: [
+            'the tissues can absorb O2 from oxyhaemoglobin',
+            'O2-concentration in tissues is higher and CO2 concentration lower as compared to lungs',
+            'oxyhaemoglobin undergoes reduction',
+            'O2-concentration in tissues is lower and CO2 concentration higher than in lungs.',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Combining of haemoglobin with O2 in lungs can be promoted by',
+        options: [
+            'decreasing O2 concentration in blood',
+            'increasing O2 concentation in blood',
+            'increasing CO2 concentration in blood',
+            'introducing CO into blood.',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The pneumotaxic centre that can moderate the function of respiratory rhythm centre is located in',
+        options: [
+            'Dorsal side of medulla',
+            'Ventral side of medulla',
+            'Aortic arch and carotid artery',
+            'Pons',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The impulse for voluntary forced breathing starts in',
+        options: ['medulla ', 'vagus nerve', 'spinal cord ', 'cerebrum'],
+        answer: 4,
+    },
+    {
+        question:
+            'Controlling centre of normal breathing (= respiratory centre) in mammals lies in',
+        options: ['cerebrum ', 'cerebellum', 'midbrain ', 'medulla oblongata'],
+        answer: 4,
+    },
+    {
+        question:
+            'What will be the pO2 and pCO2 in the atmospheric air as compared to that of alveolar air ?',
+        options: [
+            'pO2 lesser and pCO2 higher',
+            'pO2 higher and pCO2 lesser',
+            'Both pO2 and pCO2 lesser',
+            'Both pO2 and pCO2 higher',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Asthma is caused due to',
+        options: [
+            'Infection of lungs',
+            'Spasm in bronchial muscles',
+            'Bleeding into pleural cavity',
+            'infection of trachea',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Amount of O2 normally carried by 100 ml. of pure blood',
+        options: ['40 ml. ', '20 ml.', '10 ml. ', '30 ml.'],
+        answer: 2,
+    },
+    {
+        question: 'What would happen if human blood becomes acidic (low pH) ?',
+        options: [
+            'Oxygen-carying capacity of haemoglobin decreases',
+            'Oxygen-carrying capacity of haemoglobin increases',
+            'RBC count increases',
+            'RBC count decreases',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Covering of the lungs is called',
+        options: [
+            'pericardium ',
+            'perichondrium',
+            'peritoneum ',
+            'pleural membrane',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Ciliated epithelium in trachea of mammals helps in',
+        options: [
+            'sucking inspired air in',
+            'perceivings sense of smell',
+            'pushing expired air out',
+            'pushing mucus out',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The disease that occurs when the haemoglobin content of the blood goes down is –',
+        options: ['plurisy ', 'emphysema', 'anaemia ', 'pneumonia'],
+        answer: 3,
+    },
+    {
+        question: 'The affinity of CO with Hb is more than oxygen by',
+        options: ['2 times ', '20 times', '200 times ', '2000 times'],
+        answer: 3,
+    },
+    {
+        question: 'Emphysema developes mainly because of',
+        options: [
+            'Allergy or hypersensitisation',
+            'Spasm of the smooth muscles of bronchioles',
+            'Cigarette smoking',
+            'Inflammation of the alveoli',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'During respiration CO2 is transported in the form of',
+        options: [
+            'Disolved plasma',
+            'Sodium carbonate',
+            'KHCO3',
+            'Partly dissolved in plasma and partly in the form of sodium and potassium bicarbonate',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Of the following, the one which is an example of buffer system in blood is',
+        options: [
+            'Haemoglobin and oxyhaemoglobin',
+            'Oxygen and carbon dioxide',
+            'Albumin and globulin',
+            'Sodium bicarbonate and carbonic acid',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following enzymes is absent in mitochondria ?',
+        options: [
+            'Aconitase ',
+            'Malic dehydrogenase',
+            'Hexokinase ',
+            'None of these',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'If concentration of CO2 is more the curve of oxygen will shift towards',
+        options: ['Right ', 'Left', 'Central ', 'None of these'],
+        answer: 1,
+    },
+    {
+        question:
+            'The amount of volume of air that can be inspired/expired normally is called',
+        options: [
+            'Tidal volume ',
+            'Vital capacity',
+            'Residual volume ',
+            'Normal volume',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Vital capacity of the lung includes',
+        options: ['IRV + TV + ERV ', 'ERV + RV', 'ERV + TV ', 'IRV + TV'],
+        answer: 1,
+    },
+    {
+        question: 'Capacity of human lung is',
+        options: ['3000 ml ', '1500 ml', '1000 ml ', '500 ml'],
+        answer: 1,
+    },
+    {
+        question:
+            'Identify the correct statement with reference to transport of respiratory gases by blood',
+        options: [
+            'Haemoglobin is necessary for transport of carbon dioxide and carbonic anhydrase for transport of oxygen',
+            'Haemoglobin is necessary for transport of oxygen and carbonic anhydrase for transport of carbon dioxide',
+            'Only oxygen is transported by blood',
+            'Only carbon dioxide is transported by blood',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Which one of the following has the smallest diameter ?',
+        options: [
+            'Right primary bronchus',
+            'Left primary bronchus',
+            'Trachea',
+            'Respiratory bronchiole',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'The relative proportion between the volume of CO2 released and O2 absorbed in respiration is termed as',
+        options: [
+            'Respiratory exchange ',
+            'Respiratory quotient',
+            'Respiratory phase ',
+            'None of the above',
+        ],
+        answer: 2,
+    },
+];
