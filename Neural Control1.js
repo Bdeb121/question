e332f9de5626f68f3a4bc98d669751bf1a903439
Debarghya Banerjee <63992699+Bdeb121@users.noreply.github.com>
2021-07-01 15:27:30 +0530

    Create Neural Control1.js

---
+++ Neural Control1.js
@@ -0,0 +1,377 @@
+let quiz = [
+    {
+        question: 'Nerve fibres are surrounded by an insulating fatty layer called',
+        options: ['Adipose sheath ', 'Myelin sheath', 'Hyaline sheath ', 'Peritoneum'],
+        answer: 2,
+    },
+    {
+        question: 'Which of the following cell stop dividing after birth ?',
+        options: ['Epithelium ', 'Neuron', 'Glial cells ', 'Liver'],
+        answer: 2,
+    },
+    {
+        question: 'The parts of the neurons that perform basic cellular functions such as protein synthesis are the –',
+        options: ['Axons ', 'Dendrites', 'Synaptic knobs ', 'Soma'],
+        answer: 4,
+    },
+    {
+        question: 'The neurotransmitter which is not excitatory in function is–',
+        options: ['Serotonin', 'Dopamine', 'Gamma-amino butyric acid (GABA)', 'Acetyl choline'],
+        answer: 3,
+    },
+    {
+        question: 'The brain- stem includes the parts of',
+        options: ['Fore brain and mid brain', 'Mid brain and hind brain', 'Fore brain, mid brain and hind brain', 'Hind brain only'],
+        answer: 2,
+    },
+    {
+        question: 'Nerve impulses are initiated by nerve fibers only when the membrane shall become more permeable to –',
+        options: ['Adrenaline ', 'Phosphorus', 'Sodium ions ', 'Potassium ions'],
+        answer: 3,
+    },
+    {
+        question: 'At resting stage nerve cells has –',
+        options: ['Low K+ outside and high Na+ inside', 'High K+ inside and high Na+ outside', 'High K+ inside and low Na+ outside', 'High K+ outside and low Na+ inside'],
+        answer: 2,
+    },
+    {
+        question: 'Depolarization of nerve impulse involves-',
+        options: ['Entry of Na+ only', 'Entry of K+ only', 'Entry of Na+ and exit of K+', 'Entry of K+ and exit of Na+'],
+        answer: 3,
+    },
+    {
+        question: 'Node of Ranvier is found in –',
+        options: ['Muscle bundles ', 'Dendrite', 'Axon ', 'Right auricle'],
+        answer: 3,
+    },
+    {
+        question: 'Sodium – Postassium pump across membrance, actively transports',
+        options: ['2 Na ions outwards and 3 K ions into the cell', '3 Na ions outwards and 2 K ions into the cell', '2 K ions out wards and 3 Na ions into the cell', '3 K ions outwards and 2 Na ions into the cell'],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is the correct direction of travel for a nervous impulse?',
+        options: ['Dendrite, nerve cell body, axon', 'Axon, nerve cell body, dendrite', 'Nerve cell body, axon, dendrite', 'Dendrite, axon, nerve cell body'],
+        answer: 1,
+    },
+    {
+        question: 'Neurons receive signals through their __________ and send signals to other neurons through their __________.',
+        options: ['dendrites ... receptors', 'end feet ... cell bodies and dendrites', 'cell bodies and dendrites ... axons', 'transmitter vesicles ... axons'],
+        answer: 3,
+    },
+    {
+        question: 'Saltatory conduction means that the membrane potential changes _______.',
+        options: ['along the entire length of the axon', 'only where there is an imbalance between the salts present', 'in an all-or-none fashion', 'only at the nodes of Ranvier'],
+        answer: 4,
+    },
+    {
+        question: 'Mark the correct statement',
+        options: ['Electrical synapses are more common in our neural system than chemical synapses', 'The new potential in post synaptic neuron may be either excitatory or inhibitory', 'Hypothalamus is the major coordination centre for sensory and motor signaling', 'The tracts of nerve fibres that connect two cerebral hemispheres are called corpora bigemina'],
+        answer: 2,
+    },
+    {
+        question: 'The cerebral cortex is',
+        options: ['The outer layer of cerebrum, called white matter', 'Inner layer of cerebrum, called white matter', 'The outer layer of cerebrum, called grey matter', 'Inner layer of cerebrum, called grey matter'],
+        answer: 3,
+    },
+    {
+        question: 'Which part of the brain regulates the body temperature, hunger and water balance ?',
+        options: ['Hypothalamus ', 'Infundibulum', 'Medulla oblongata ', 'Pons veroli'],
+        answer: 1,
+    },
+    {
+        question: 'Which of the following forms the cerebro spinal fluid?',
+        options: ['Choroid plexus ', 'Duramater', 'Arachnoid mater ', 'Cerebrum and spinal cord'],
+        answer: 1,
+    },
+    {
+        question: 'Arbor vitae is composed of',
+        options: ['Neuroglea cells ', 'Grey matter', 'White matter ', 'All of these'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following is a richly vascular layer with lots of blood capillaries ?',
+        options: ['Duramater of brain', 'Piamater of spinal cord', 'Epidermis of skin', 'Epithelial lining of trachea'],
+        answer: 2,
+    },
+    {
+        question: 'The secretion of gastric juice is controlled by',
+        options: ['Cerebellum ', 'ANS', 'Cerebrum ', 'Medulla'],
+        answer: 4,
+    },
+    {
+        question: 'The energy required during transmission of impulse is provided by –',
+        options: ['Brain ', 'Nerve fiber', 'Liver ', 'Stimulating agent'],
+        answer: 2,
+    },
+    {
+        question: 'Which part of the brain is more developed in human ?',
+        options: ['Medulla ', 'Cerebellum', 'Cerebrum ', 'Optic lobes'],
+        answer: 3,
+    },
+    {
+        question: 'In a human being, number of cranial nerves are –',
+        options: ['10 pairs ', '20 pairs', '6 pairs ', '12 pairs'],
+        answer: 4,
+    },
+    {
+        question: 'A thin vascular membrane of brain is –',
+        options: ['Arachnoid ', 'Piameter', 'Durameter ', 'Epineurium'],
+        answer: 2,
+    },
+    {
+        question: 'The cerebellum is concerned with:',
+        options: ['memory', 'perception', 'intelligence', 'coordination of muscular movement'],
+        answer: 4,
+    },
+    {
+        question: 'Twilight vision is also called',
+        options: ['Scotopic vision and is the function of rods', 'Scotopic vision and is the function of cones', 'Photopic vision and is the function of rods', 'Photopic vision and is the function of cones'],
+        answer: 1,
+    },
+    {
+        question: 'Energy transformation in nervous system is-',
+        options: ['Chemical to radiant', 'Chemical to mechanical', 'Chemical to electrical', 'Chemical to osmotic'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following pair is mismatched?',
+        options: ['Cerebrum-Memory', 'Cerebellum-Equilibrium of body', 'Olfactory lobes - Smell', 'Medulla - Temperature'],
+        answer: 4,
+    },
+    {
+        question: 'Name of cranial nerves of human being viz. II, VII, VIII, IX',
+        options: ['Optic, auditory, facial, hypoglossal', 'Oculomotor, auditory, abducens, hypoglossal', 'Optic, facial, auditory, glossopharyngeal', 'Optic, facial, abducens, glossopharyngeal'],
+        answer: 3,
+    },
+    {
+        question: 'Several nervous system diseases result from a loss of support cells or of substances produced by support cells (such as myelin). The primary function of these support cells is to',
+        options: ['act as supporting structures within nervous tissue.', 'produce insulating material around axons.', 'assist in the conduction of impulses along neurons.', 'All of the above'],
+        answer: 4,
+    },
+    {
+        question: 'Homeostasis is governed by complex interactions within the',
+        options: ['hypothalamus ', 'pituitary gland', 'thalamas ', 'All of these'],
+        answer: 4,
+    },
+    {
+        question: 'The white matter of the CNS is always',
+        options: ['deep to the gray matter', 'unmyelinated', 'arranged into tracts', 'composed of sensory fibers only'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following is not a part of forebrain ?',
+        options: ['Rhiencephalon ', 'Rhombencephalon', 'Diencephalon ', 'Telencephalon'],
+        answer: 2,
+    },
+    {
+        question: 'Clusters of neuron cell bodies embedded in the white matter of the brain are referred to as',
+        options: ['Nuclei ', 'Gyri', 'Sulci ', 'Ganglia'],
+        answer: 1,
+    },
+    {
+        question: 'Which part of the brain controls involuntary breathing ?',
+        options: ['Diencephalon ', 'Hypothalamus', 'Medulla oblongata ', 'Cerebellum'],
+        answer: 3,
+    },
+    {
+        question: 'If a person has lost his memory in an accident, the following part of the brain have got injured',
+        options: ['Diencephalon ', 'Medulla oblongata', 'Cerebellum ', 'Cerebrum'],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following cranial nerve of man is both sensory and motor?',
+        options: ['Olfactory ', 'Trigeminal', 'Optic ', 'Auditory'],
+        answer: 2,
+    },
+    {
+        question: 'Which of the following cranial nerves are involved in the movement of eye?',
+        options: ['Optic, occulomotor, abducens', 'Occulomotor, abducens, trochlear', 'Trochlear, abducens and optic', 'Abducens, optic, trochlear, occulomotor'],
+        answer: 2,
+    },
+    {
+        question: 'The smallest cranial nerve in human being is',
+        options: ['Trochlear ', 'Opthalmic', 'Abducens ', 'Vagus'],
+        answer: 1,
+    },
+    {
+        question: 'The part of human hind brain that is responsible for hand-eye coordination is',
+        options: ['Cerebellum ', 'pons varolii', 'Medulla oblongata ', 'Thalamus'],
+        answer: 1,
+    },
+    {
+        question: 'The pneumotaxic centre in the body is',
+        options: ['Heart ', 'Lung', 'Medulla ', 'Liver'],
+        answer: 3,
+    },
+    {
+        question: 'The pathway of reflex arc as :',
+        options: ['sense organ, spinal cord, motor neuron, sensory nerve, muscle', 'sense organ, sensory, neuron, motor neuron, spinal cord, muscle', 'sense organ, motor neuron, spinal cord, sensory neuron, muscle', 'sense organ, motor neuron, spinal cord, sensory neuron, muscle'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following statements about the neural basis of the knee-jerk reflex in humans is false ?',
+        options: ['The leg extensor is stimulated to contract by the motor neuron in this reflex loop.', 'The only synapses between neurons involved in this loop occur in the spinal cord.', 'The basic response is a polysynaptic circuit containing several interneurons.', 'Conscious action can modify the basic reflex because of the action of interneurons.'],
+        answer: 3,
+    },
+    {
+        question: 'Cerebellum of brain is',
+        options: ['concerned with the maintenance of posture/equilibrium', 'responsible for olfactory functions', 'controls optic functions', 'both a and c'],
+        answer: 1,
+    },
+    {
+        question: 'Identify the organ/innervation mismatch',
+        options: ['Glossopharyngeal nerve-tongue', 'Optic nerve-Eye', 'Facial nerve-Olfactory epithelium', 'Cochlear nerve-Spiral organ'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following correctly traces the energy of sound waves into the ear?',
+        options: ['Auditory canal-eardrum-ear bones-cochlea', 'Eardrum-auditory canal-cochlea-ear bones', 'Auditory canal-ear bones-eardrum-cochlea', 'Eardrum-auditory canal-ear bones-cochlea'],
+        answer: 1,
+    },
+    {
+        question: 'Aqueous humour produced by the ciliary body is secreted into the posterior chamber and enters the anterior chamber through',
+        options: ['the pupil ', 'the scleral venous sinus', 'the vitreous body ', 'the suspensory ligament'],
+        answer: 1,
+    },
+    {
+        question: 'Reflex action is controlled by',
+        options: ['Sympathetic nervous system', 'Autonomic nervous system', 'Spinal cord', 'Peripheral nervous system'],
+        answer: 3,
+    },
+    {
+        question: 'Which one of the following reflex do not include innervation of brain?',
+        options: ['Spinal reflex ', 'Cranial reflex', 'Afferent reflex ', 'Efferent reflex'],
+        answer: 1,
+    },
+    {
+        question: 'Which is not a reflex action?',
+        options: ['Swallowing of food', 'Shivering in cold', 'Salivation at choicest food', 'Closure of eyelid by flashing light'],
+        answer: 2,
+    },
+    {
+        question: 'Perception of colour is possible among',
+        options: ['Birds ', 'Reptiles', 'Men ', 'Both a and c'],
+        answer: 4,
+    },
+    {
+        question: 'Aqueous humour is present',
+        options: ['Infront of the retina ', 'Infront of the cornea', 'Behind the conjunctiva ', 'Infront of the lens'],
+        answer: 4,
+    },
+    {
+        question: 'In the blind spot where the optic nerves leave the eye',
+        options: ['Rods and cones are absent', 'Only cones are present', 'Only rods are present', 'Special neurons are present'],
+        answer: 1,
+    },
+    {
+        question: 'In the yellow spot or fovea centralis, the cells present are',
+        options: ['Rods and cones ', 'Only rods', 'Only cones ', 'Predominantly rods'],
+        answer: 3,
+    },
+    {
+        question: 'The rods and cones of the eye retinal layer are modified',
+        options: ['Hair ', 'Unipolar neurons', 'Bipolar neurons ', 'Multipolar neurons'],
+        answer: 3,
+    },
+    {
+        question: 'In mammalian eye, the power of accomodation is controlled by changing the shape of the lens. It is governed by',
+        options: ['Cornea ', 'Pupil', 'Iris ', 'Ciliary body'],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following disorder of eyes caused by UV radiations?',
+        options: ['Cataract ', 'Glaucoma', 'Dilation pupil ', 'Some defect of retina'],
+        answer: 1,
+    },
+    {
+        question: 'Bipolar neurons occur in',
+        options: ['Vertebrate embryos ', 'Retina of eye', 'Brain and spinal cord ', 'Skeletal muscles'],
+        answer: 2,
+    },
+    {
+        question: 'The potential diference between outside and inside of a nerve before excitation is known as',
+        options: ['Spike potential ', 'Reaction potential', 'Action potential ', 'Resting potential'],
+        answer: 4,
+    },
+    {
+        question: 'The human eye is sensitive only to light having wavelength ranging from',
+        options: ['80 to 280 nanometre ', '380 to 760 nanometre', '780 to 870 nanometre ', '880 to 980 nanometre'],
+        answer: 2,
+    },
+    {
+        question: 'In the myopia eye defect, the rays of light',
+        options: ['Do not enter the eye at all', 'Come to a focus at back of retina', 'Come to a focus in front of the retina', 'Come to a focus in between retina & iris'],
+        answer: 3,
+    },
+    {
+        question: 'Our ear can hear the frequency of sound waves',
+        options: ['20 to 20,000 cycles/sec', '1000 to 2000 cycles/sec', '5000 to 7000 cycles/sec', '5000 to 10,000 cycles/sec'],
+        answer: 1,
+    },
+    {
+        question: 'Ear drum is know as',
+        options: ['Tympanic membrane ', 'Tensor tympani', 'Scala tympani ', 'Scala vestibuli'],
+        answer: 1,
+    },
+    {
+        question: 'If the light source infront of an eye becomes bright suddenly',
+        options: ['Focus of lens will change', 'Retinal blood supply will cut', 'Vitreous humour becomes fluid', 'Pupil will contract'],
+        answer: 4,
+    },
+    {
+        question: 'The small canal leading from pinna into the head in mammals is',
+        options: ['Haversian canal ', 'Ext. auditory maetus', 'Inguinal canal ', 'Eustachian tube'],
+        answer: 2,
+    },
+    {
+        question: 'Stereoscopic vision is found in –',
+        options: ['All mammals ', 'All vertebrates', 'Primates ', 'Frog'],
+        answer: 3,
+    },
+    {
+        question: 'Fenestra ovalis is the opening of –',
+        options: ['Cranium ', 'Tympanum', 'Tympanic cavity ', 'Brain'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following food is needed for the synthesis of rhodopsin.',
+        options: ['Mango ', 'Rice', 'Carrot ', 'Tomatoes'],
+        answer: 3,
+    },
+    {
+        question: 'No image formation occurs on blind-spot of retina because',
+        options: ['It is not present on the optical axis of the eye', 'Here cones and rods are absent', 'On this part only cones are present', 'The nerve fibres of this region do not contribute in the formation of optic chiasma'],
+        answer: 2,
+    },
+    {
+        question: '"Telescopic vision" found in –',
+        options: ['Amphibians ', 'Mammals', 'Birds ', 'None of these'],
+        answer: 3,
+    },
+    {
+        question: 'A circular canal which is found in limbus part of eyes is called –',
+        options: ['Hyaloid canal ', 'Canal of Schlemm', 'Canal of Cloquet ', 'Eustachian tube'],
+        answer: 2,
+    },
+    {
+        question: 'Middle ear is filled with –',
+        options: ['Air ', 'Endolymph', 'Perilymph ', 'Cerebrospinal fluid'],
+        answer: 1,
+    },
+    {
+        question: 'Three layers in eye ball from inside out are –',
+        options: ['Retina, choroid, sclerotic ', 'Choroid, retina, sclerotic', 'Sclerotic, choroid, retina ', 'Sclerotic, retina, choroid'],
+        answer: 1,
+    },
+    {
+        question: 'Aqueous humor and vitreous humor are secreted by',
+        options: ['Iris ', 'Ciliary body', 'Lens ', 'Cornea'],
+        answer: 2,
+    },
+    {
+        question: 'Which of the following pair(s) is/are correct?',
+        options: ['Semicircular canal - Balancing', 'Cochlea - Hearing', 'Utriculus & sacculus - Balancing & hearing', 'All of the above'],
+        answer: 4,
+    }
+];
