45217313472035d0f491b288c26e9cb499fde202
Debarghya Banerjee <63992699+Bdeb121@users.noreply.github.com>
2021-07-01 15:29:15 +0530

    Create Locomotion1.js

---
+++ Locomotion1.js
@@ -0,0 +1,435 @@
+let quiz = [
+    {
+        question: 'Which are involved in muscle contraction?',
+        options: [
+            'Ca2+ and Mg2+ ',
+            'Ca2+ and Na+',
+            'Na+ and K+ ',
+            'Mg2+ and K+',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The contractile protein of muscle is-',
+        options: ['Tubulin ', 'Myosin', 'Tropomyosin ', 'Actin'],
+        answer: 4,
+    },
+    {
+        question: 'Which one yields ATP required for muscle contraction ?',
+        options: [
+            'Myoglobin ',
+            'Creatine phosphate',
+            'Creatinine phosphate ',
+            'Myosin',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'During contraction of muscles:',
+        options: [
+            'Actin filament slide over actin',
+            'Myosin filament slide over actin',
+            'Actin filament slide over myosin',
+            'Myosin filament slide over actin',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The macrophages in human body exhibit',
+        options: [
+            'Ciliary movement',
+            'Amoeboid movement',
+            'No movement',
+            'Movement with the blood flow only',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Which of the following is not the feature of red muscle fibres?',
+        options: [
+            'They have plenty of mitochondria',
+            'They have high content of myoglobin',
+            'They have high amount of sarcoplasmic reticulum',
+            'They are called aerobic muscles',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'ATPase activity found in :',
+        options: [
+            'Myosin filament ',
+            'Actin filament',
+            'Both ',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Total No. of muscles in our body is',
+        options: ['256 muscles ', '639 muscles', '400 muscles ', '421 muscles'],
+        answer: 2,
+    },
+    {
+        question:
+            'The muscle band that remains unchanged during contraction and relaxation of the skeletal muscle is',
+        options: ['I ', 'H', 'A ', 'Z line'],
+        answer: 3,
+    },
+    {
+        question:
+            'The lactic acid generated during muscle contraction is converted to glycogen in :',
+        options: ['Muscle ', 'Kidney', 'Pancreas ', 'Liver'],
+        answer: 4,
+    },
+    {
+        question: 'Mark the incorrect statement in the followings',
+        options: [
+            'All movements lead to locomotion',
+            'Ciliary movement help in passage of ova through female reproductive tract',
+            'Microfilaments are involved in amoeboid movement',
+            'In Paramecium the cilia help in movement of food through cytopharynx and in locomotion as well',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'The strength of contraction of an entire skeletal muscle is dependent on the',
+        options: [
+            'Number of muscle fibres that contract simultaneously',
+            'Frequency of contraction of each muscle fibre',
+            'Number of active cross bridges in each muscle fibre',
+            'All of the above',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Ribs are',
+        options: [
+            'Membranous bones ',
+            'Sesamoid bones',
+            'Cartilagenous bones ',
+            'Dermal bones',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Heart muscle is sensitive to',
+        options: [
+            'electrical stimuli ',
+            'chemical stimuli',
+            'mechanical stimuli ',
+            'all of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'The joint found between sternum and the ribs in humans is',
+        options: [
+            'angular joint ',
+            'fibrous joint',
+            'cartilaginous joint ',
+            'gliding joint',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Read the following 4-statements (A – D) and accordingly mark the option that has both correct statements',
+        statement: [
+            'A. Cardiac fibres are branched with one or more nuclei',
+            'B. Smooth muscles are unbranched and cylindrical',
+            'C. Striated muscles can be branched or unbranched',
+            'D. Involuntary muscles are non-striated',
+        ],
+        options: ['A and D', 'B and C', 'C and D', 'A and C'],
+        answer: 4,
+    },
+    {
+        question: 'What is not true about human skull ?',
+        options: [
+            'It is dicondylic',
+            'It includes 6 ear ossicles',
+            'It includes 14 facial bones',
+            'Hyoid is not included in skull bones',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which is part of pectoral girdle?',
+        options: ['Glenoid cavity ', 'Sternum', 'Ilium ', 'Acetabulum'],
+        answer: 1,
+    },
+    {
+        question: 'Ankle joint is –',
+        options: [
+            'Pivot joint ',
+            'Ball and socket joint',
+            'Hinge joint ',
+            'Gliding joint',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Number of bones in hind limb of human is-',
+        options: ['21 ', '24', '30 ', '14'],
+        answer: 3,
+    },
+    {
+        question: 'Biceps are attached with-',
+        options: ['Radius ', 'Ulna', 'Femur ', 'Humerus'],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is not the bone of cranium ?',
+        options: [
+            'Occipital bone ',
+            'Zygomatic bone',
+            'Ethmoid bone ',
+            'Sphenoid bone',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Sutures present between various bones of skull are –',
+        options: [
+            'Cartilagenous joints ',
+            'Synovial joints',
+            'Hinge joints ',
+            'Fibrous joints',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Joints between carpals are –',
+        options: [
+            'Fibrous joints ',
+            'Cartilagenous joints',
+            'Angular joints ',
+            'Gliding joints',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Joint between atlas and axis is-',
+        options: [
+            'Pivot joint ',
+            'Saddle joint',
+            'Angular joint ',
+            'Hinge joint',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'In which option the number of bones of two corresponding parts are not the same',
+        options: [
+            'Thigh and upper arm ',
+            'Sole and Palm',
+            'Ankle and wrist ',
+            'Leg and arm',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'The clavicle or collar bone articulates with',
+        options: [
+            'Glenoid cavity ',
+            'Cervical vertebrae',
+            'Coxal bone ',
+            'Acromian process',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Sutural joints are present between-',
+        options: [
+            'Thumb and metatarsal',
+            'Humerus and radio-ulna',
+            'Parietals of skull',
+            'Glenoid cavity and pectoral girdle',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Joint of sternum and ribs is-',
+        options: [
+            'Cartilaginous ',
+            'Fibrous joint',
+            'Angular joint ',
+            'Hinge joint',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Largest synovial joint is –',
+        options: ['Hip joint ', 'Knee joint', 'Shoulder joint ', 'Ankle joint'],
+        answer: 2,
+    },
+    {
+        question: 'Haversian system is diagnostic feature of-',
+        options: [
+            'Avian bones ',
+            'Reptilian bones',
+            'Mammalian bones ',
+            'Bones of all animal',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following is an autoimmune disorder ?',
+        options: [
+            'Myasthenia gravis',
+            'Osteoporosis',
+            'Muscular dystrophy',
+            'Gout',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Mark the correct statement',
+        options: [
+            'All striated muscles are voluntary',
+            'Visceral muscles are faintly striated',
+            'Cardiac muscles are not striated',
+            'All non-striated muscles are involuntary',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'In children the bones are more flexible and brittle because their bones have',
+        options: [
+            'large quantity of salts and little organic substances',
+            'large quantity of organic substances and little salts',
+            'well developed haversian system',
+            'large number of osteoblasts',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Which of the following is the contractile protein of a muscle?',
+        options: ['Myosin ', 'Tropomyosin', 'Actin ', 'Tubulin'],
+        answer: 1,
+    },
+    {
+        question: 'In mammals, the number of cervical vertebra is',
+        options: ['5 ', '7', '10 ', '12'],
+        answer: 2,
+    },
+    {
+        question: 'Which one is odd pair ?',
+        options: [
+            'Femur-Humerus ',
+            'Tibia-Radius',
+            'Axis-Atlas ',
+            'Tarsal-Carpals',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Cranium of man is made up of',
+        options: ['8 bones ', '12 bones', '10 bones ', '16 bones'],
+        answer: 1,
+    },
+    {
+        question:
+            'How many ball and socket joints are present in a human body?',
+        options: ['2 ', '4', '8 ', '6'],
+        answer: 4,
+    },
+    {
+        question: 'The joint between incus and stapes is',
+        options: [
+            'Hinge joint ',
+            'Gliding joint',
+            'Pivotal joint ',
+            'Ball and socket joint',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Pneumatic bones are found in',
+        options: ['House lizard ', 'Pigeon', 'Flying fish ', 'Frog’s tadpole'],
+        answer: 2,
+    },
+    {
+        question:
+            'The cross arm that forms the cross bridges during muscle contraction, is formed by',
+        options: ['HMM ', 'LLM', 'Troponin ', 'Both 1 and 2'],
+        answer: 1,
+    },
+    {
+        question: 'The coxal bone consists of',
+        options: [
+            'Clavicle, coracoid and scapula',
+            'Maxilla, jugal and squamosal',
+            'Ilium, ischium and pubis',
+            'Two clavicles and one interclavicle',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'A cricket player is fast chasing a ball in the field. Which one of the following groups of bones are directly contributing in this movement?',
+        options: [
+            'Femur, malleus, tibia, metatarsals',
+            'Pelvis, ulna, patella, tarsals',
+            'Sternum, femur, tibia, fibula',
+            'Tarsals, femur, metatarsals, tibia',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following pairs, is correctly matched?',
+        options: [
+            'Hinge joint - between vertebrae',
+            'Gliding joint - between zygapophyses of the successive vertebrae',
+            'Cartilaginous joint - skull bones',
+            'Fibrous joint - between phalanges',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Following is the store house of calcium in muscles',
+        options: [
+            'Sarcosome ',
+            'Sarcoplasmic reticulum',
+            'Creatine phosphate ',
+            'Sarcomere',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Actin protein occurs in two forms',
+        options: [
+            'Polymeric F- actin and monomeric G- actin',
+            'Monomeric F- actin and polymeric G-actin',
+            'The tail and a head',
+            'F-actin and G- actin, but both globular',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Total number of movable bones (excluding hyoi4 in human skull are',
+        options: ['1 ', '4', '6 ', '7'],
+        answer: 1,
+    },
+    {
+        question: 'In old age stiffness of joints is due to the –',
+        options: [
+            'Hardening of bones',
+            'Inefficiency of muscles',
+            'Decrease in synovial fluid',
+            'Enlargement of bones',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Lumbar vertebra are found in',
+        options: ['Neck region ', 'Abdominal region', 'Hip region ', 'Thorax'],
+        answer: 2,
+    },
+];
