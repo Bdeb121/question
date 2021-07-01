7ae97b6e2264e6f3a2d8afa4d8302e569b012020
Debarghya Banerjee <63992699+Bdeb121@users.noreply.github.com>
2021-07-01 15:23:52 +0530

    Create Chemical Coordination1.js

---
+++ Chemical Coordination1.js
@@ -0,0 +1,252 @@
+let quiz = [
+    {
+        question: 'Glycogenesis is the result of the hormone of',
+        options: ['alpha cells of pancreas ', 'beta cells of pancreas', 'thyroid gland ', 'adrenal gland'],
+        answer: 2,
+    },
+    {
+        question: 'Which of the following is not a steroid hormone ?',
+        options: ['Androgen ', 'Aldosterone', 'Estrogen ', 'Relaxin'],
+        answer: 4,
+    },
+    {
+        question: 'Endocrine glands produce or action of endocrine glands is mediated through',
+        options: ['Hormones ', 'Enzymes', 'Minerals ', 'Vitamins'],
+        answer: 1,
+    },
+    {
+        question: 'Which of the following is the function of testosterone ?',
+        options: ['It stimulates muscular growth and aggressiveness', 'It influences male sexual behaviour (libido)', 'It causes anabolic (syntheti3 effect on protein/carbohydrate metabolism', 'All of these'],
+        answer: 3,
+    },
+    {
+        question: 'Vasopressin stimulates reabsorption of water and reduction of urine secretion. Hence vasopressin is otherwise called',
+        options: ['synovial fluid', 'neurotransmitter', 'antidiuretic hormone', 'growth regulating substance'],
+        answer: 3,
+    },
+    {
+        question: 'Identify from the following, a hormone produced by the pituitary gland in both males and females but functional only in females',
+        options: ['vasopressin ', 'relaxin', 'prolactin ', 'somatotropic hormone'],
+        answer: 3,
+    },
+    {
+        question: 'A hormone that acts upon the renal tubule and blood capillaries is:',
+        options: ['glucagon ', 'aldosterone', 'vasopressin ', 'glucocorticoids'],
+        answer: 2,
+    },
+    {
+        question: 'ANF is a peptide hormone and is secrected from',
+        options: ['Gastrointestinal tract ', 'Kidney', 'Post. Pituitary ', 'None of these'],
+        answer: 4,
+    },
+    {
+        question: 'Calcitonin is a thyroid hormone which',
+        options: ['lowers calcium level in blood', 'elevates calcium level in blood', 'has no effect on calcium', 'elevates potassium level in blood'],
+        answer: 1,
+    },
+    {
+        question: 'Which of the following endocrine gland stores its secretion in the extracellular space before discharging it into the blood?',
+        options: ['Pancreas ', 'Adrenal', 'Testis ', 'Thyroid'],
+        answer: 4,
+    },
+    {
+        question: 'The hormone responsible for the regulation of metabolism of calcium and phosphorus is secreted by',
+        options: ['Thyroid', 'Parathyroid and thyroid both', 'Thymus', 'Pancreas'],
+        answer: 2,
+    },
+    {
+        question: 'Which hormone acts on exocrine part of pancreas?',
+        options: ['GIP ', 'Insulin', 'Secretin ', 'Steapsin'],
+        answer: 3,
+    },
+    {
+        question: 'Progesterone, the component of the oral contraceptive pills, prevents pregnancy by',
+        options: ['preventing the cleavage of the fertilized egg', 'preventing the formation of ova', 'blocking ovulation', 'creating unfavourable chemical environment for the sperms to survive in the female reproductive tract'],
+        answer: 3,
+    },
+    {
+        question: 'Melanocyte stimulating hormone (MSH) is produced by',
+        options: ['parathyroid', 'pars intermedia of pituitary', 'anterior pituitary', 'posterior pituitary'],
+        answer: 2,
+    },
+    {
+        question: 'Melatonin is produced by',
+        options: ['thymus ', 'skin', 'pituitary ', 'pineal gland'],
+        answer: 4,
+    },
+    {
+        question: 'The hormone that supports pregnancy and stimulates mammary glands for the formation of alveoli for storing milk, is secreted from',
+        options: ['Ant. Pituitary ', 'Post. Pituitary', 'Graafian follicle ', 'Corpus luteum'],
+        answer: 4,
+    },
+    {
+        question: 'Mammals born without a thymus gland fail to manufacture–',
+        options: ['B - Lymphocytes ', 'T - Lymphocytes', 'Plasma cells ', 'Basophils'],
+        answer: 2,
+    },
+    {
+        question: 'Which of the endocrine gland is mainly concerned with immunity in man ?',
+        options: ['Parathyroid gland ', 'Adrenal gland', 'Thymus gland ', 'Posterior pituitary gland'],
+        answer: 3,
+    },
+    {
+        question: 'A gland which gradually atrophies at the age of 14- 16 due to the activities of sex gland is',
+        options: ['thyroid ', 'parathyroid', 'pancreas ', 'thymus'],
+        answer: 4,
+    },
+    {
+        question: 'Adrenaline directly affects on',
+        options: ['S. A. node', 'a-cells of Langerhans', 'dorsal root of spinal cord', 'epithelial cells of stomach'],
+        answer: 1,
+    },
+    {
+        question: 'The hormone that suppresses the immune system is',
+        options: ['Glucagon ', 'Thymosin', 'Adrenaline ', 'Cortisol'],
+        answer: 4,
+    },
+    {
+        question: 'Epinephrine is –',
+        options: ['Secreted from pancreas and decreases heart beat', 'Secreted from adrenal medulla and increases heart beat', 'Secreted from adrenal medulla and decreases heart beat', 'Secreted from pancreas and increases heart beat'],
+        answer: 2,
+    },
+    {
+        question: 'Retention of sodium in body depends up on hormone from–',
+        options: ['Adrenal cortex ', 'Adrenal medulla', 'Parathyroid ', 'Thyroid'],
+        answer: 1,
+    },
+    {
+        question: 'Both adrenaline and cortisol are secreted in response to stress. Which of the following statements is true for both of these hormones ?',
+        options: ['They act to increase blood glucose', 'They are secreted by the adrenal cortex', 'Their secretion is stimulated by adrenocorticotropin', 'They are secreted into the blood within seconds of the onset of stress.'],
+        answer: 1,
+    },
+    {
+        question: 'Thymosin stimulates –',
+        options: ['Milk secretion ', 'Erythrocytes', 'T-lymphocytes ', 'Melanocytes'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following diseases is caused by the under secreation of cortisol?',
+        options: ['Anaemia', 'Addison’s disease', 'Hyperglycemia', 'Mental illness or retardation'],
+        answer: 2,
+    },
+    {
+        question: 'Steroid hormones –',
+        options: ['have only cell surface receptors', 'are lipophobic', 'act through altering the activity of proteins in the target cell', 'are produced by only adrenal cortex.'],
+        answer: 3,
+    },
+    {
+        question: 'The urine of a man is very dilute and the quantity of urine is too much and dehydration has started in his body and he is very thirsty by the cause of',
+        options: ['Hypesecretion of ADH ', 'Hyposecretion of ADH', 'Both and ', 'None of these'],
+        answer: 2,
+    },
+    {
+        question: 'Which hormone promotes cell division, protein synthesis and bone growth?',
+        options: ['GH (STH) ', 'PTH', 'LH ', 'ACTH'],
+        answer: 1,
+    },
+    {
+        question: 'Which of the following endocrine glands functions under nervous control',
+        options: ['Cortex of adrenal glands', 'Medulla of adrenal glands', 'Anterior pituitary glands', 'Posterior pituitary gland'],
+        answer: 2,
+    },
+    {
+        question: 'Which hormone is related to mineral metabolism but is not a peptide / protein in nature',
+        options: ['PTH ', 'ANF', 'Aldosterone ', 'All of these'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following is not under direct control of pituitary gland with respect to the regulation of its secretory function?',
+        options: ['Adrenal cortex ', 'Adrenal medulla', 'Thyroid ', 'Testis'],
+        answer: 2,
+    },
+    {
+        question: 'Secretin stimulates the activity of :',
+        options: ['Liver ', 'Gastric gland', 'Pancreas ', 'Gall-bladder'],
+        answer: 3,
+    },
+    {
+        question: 'Cholecystokinin and secretin are',
+        options: ['Hormones liberated by mucosa of duodenum and stimulate gall bladder and pancreas respectively', 'Hormones stimulating liver', 'Hormones stimulating pancreas', 'Enzymes'],
+        answer: 1,
+    },
+    {
+        question: 'During menstruation the level of progesterone in the blood is',
+        options: ['Low ', 'Normal', 'High ', 'Very high'],
+        answer: 1,
+    },
+    {
+        question: 'Which one of the following is both hormone and enzyme?',
+        options: ['ADH hormone ', 'Acetylcholinesterase', 'Angiotensinogen ', 'Renin'],
+        answer: 4,
+    },
+    {
+        question: 'The target organ of insulin and adrenaline is:',
+        options: ['heart ', 'lungs', 'liver ', 'pancreas'],
+        answer: 2,
+    },
+    {
+        question: 'Mainly which type of hormones control the menstrual cycle in human beings?',
+        options: ['FSH ', 'LH', 'FSH, LH, estrogen ', 'Progesterone'],
+        answer: 3,
+    },
+    {
+        question: 'In human adult females oxytocin',
+        options: ['is secreted by anterior pituitary', 'stimulates growth of mammary glands', 'stimulates pituitary to secrete vasopressin', 'causes strong uterine contractions during parturition'],
+        answer: 4,
+    },
+    {
+        question: 'Glucagon is secreted from which cells of Islets of Langerhans?',
+        options: ['a-cells ', 'b-cells', 'g-cells ', 'Both a and b-cells'],
+        answer: 1,
+    },
+    {
+        question: 'Hormone connected with increased rate of glycogenesis, blood pressure and heart beat is',
+        options: ['Insulin ', 'Glucagen', 'Adrenaline ', 'FSH'],
+        answer: 3,
+    },
+    {
+        question: 'If ovaries from a pregnant woman are removed in 4th month of pregnancy then –',
+        options: ['Development of embryo becomes abnormal', 'Abortion occurs after some time', 'Embryo develops normally till birth', 'None of these'],
+        answer: 3,
+    },
+    {
+        question: 'The persistence of corpus luteum during pregnancy is due to a hormone known as',
+        options: ['Chorionic gonadotropic hormone', 'FSH', 'Estrogen', 'Progesterone'],
+        answer: 1,
+    },
+    {
+        question: 'Which one of the following is temporary endocrine gland?',
+        options: ['Pineal ', 'Pancreas', 'Placenta ', 'Parathyroid'],
+        answer: 3,
+    },
+    {
+        question: 'If a human female stats developing male characteristics like beared, degeneration of uterus and ovaries, enlargement of clitoris etc. It may be due to',
+        options: ['Over production oestrogen and testosterone', 'Damage to posterior pituitary', 'Over production of adrenal androgens', 'Surgical removal of mammary gland'],
+        answer: 3,
+    },
+    {
+        question: 'Function of renin is:',
+        options: ['to reduce blood pressure', 'vasodilation', 'degradation of angiotensinogen to angiotensin-II', 'stimulation of copious urination'],
+        answer: 3,
+    },
+    {
+        question: 'In the homeostatic control of blood sugar level, which organs function respectively as modulator and effector',
+        options: ['Liver and islets of langerhans', 'Hypothalamus and liver', 'Hypothalamus and islets of langerhans', 'Islets of langerhans and hypothalamus'],
+        answer: 3,
+    },
+    {
+        question: 'One of the following endocrine gland functions as a biological clock and a neurosecretory transducer',
+        options: ['Adrenal gland ', 'Thyroid gland', 'Pineal gland ', 'Thymus gland'],
+        answer: 3,
+    },
+    {
+        question: 'The hormones that initiate ejection of milk, stimulates milk production and growth of ovarian follicles are respectively known as',
+        options: ['PRL, OT and LH ', 'OT, PRL and FSH', 'LH, PRL and FSH ', 'PRH, OT and LH'],
+        answer: 2,
+    },
+    {
+        question: 'Hypothyroidism in adults and hyperparathyroidism will respectively lead to',
+        options: ['Myxoderma and Cretinism', "Grave's disease and Hashimoto's disease", 'Myxoedema and Osteitis fibrosa cystica', "Addison's disease and Cretinism"],
+        answer: 3,
+    } 
+];
