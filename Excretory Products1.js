2b53ffcb673ba2940f86815fc4c42049057dd1f1
Debarghya Banerjee <63992699+Bdeb121@users.noreply.github.com>
2021-07-01 15:30:32 +0530

    Create Excretory Products1.js

---
+++ Excretory Products1.js
@@ -0,0 +1,445 @@
+let quiz = [
+    {
+        question: 'The Glomerulus along with Bowman’s capsule is called',
+        options: [
+            'Column of Bertini ',
+            'Renal capsule',
+            'Malpighian tubule ',
+            'None of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is not the part of renal tubule?',
+        options: [
+            'Henle’s loop ',
+            'Glomerulus',
+            'Bowmans’s capsule ',
+            'Collecting tubule',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Waste products of adenine and guanine metabolism are excreted by man as',
+        options: ['Ammonia ', 'Urea', 'Uric acid ', 'Allantois'],
+        answer: 3,
+    },
+    {
+        question:
+            'A man takes large amount of protein. He is likely to excrete more amount of',
+        options: ['water ', 'glucose', 'urea and uric acid ', 'salts '],
+        answer: 3,
+    },
+    {
+        question: 'In man, the urea is mainly produced in',
+        options: ['Liver ', 'Kidneys', 'Gall bladder ', 'Spleen'],
+        answer: 1,
+    },
+    {
+        question: 'Aquatic reptiles are',
+        options: [
+            'Ammonotelic ',
+            'Ureotelic over land',
+            'Ureotelic ',
+            'Ureotelic in water',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Which one is not correct ?',
+        options: [
+            'Humans - Uriotelic ',
+            'Birds - Uricotelic',
+            'Lizards - Uricotelic ',
+            'Whale - Ammonotelics',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Which of the following animal dose not excrete nitrogen as urea?',
+        options: ['Tadpole ', 'Frog', 'Toad ', 'Elesmobranch fish'],
+        answer: 1,
+    },
+    {
+        question:
+            'Uric acid is the chief nitrogenous component of the excretory products of',
+        options: ['Man ', 'Earthworm', 'Cockroach ', 'Frog'],
+        answer: 3,
+    },
+    {
+        question: 'Which of the following hormone is secreted from kidney?',
+        options: ['ANF ', 'Erythropoetin', 'Rennin ', 'Aldosterone'],
+        answer: 2,
+    },
+    {
+        question:
+            'The appearance of albumin in the urine is most likely due to',
+        options: [
+            'Increase in the blood pressure',
+            'Decrease in the blood osmotic corpuscles',
+            'Damage to the Malpighian corpuscles',
+            'Damage to the proximal convoluted tubules',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'The blood constituents that remain unchanged in quantity after circulating through the kidney are',
+        options: [
+            'Urea and glucose ',
+            'Glucose and proteins',
+            'Urea and proteins ',
+            'Urea and uric acid',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            "Due to insufficient filtration in the Bowman's capsule, all are likely to happen except",
+        options: [
+            'Accumulation of fluid in the body',
+            'Increase in blood pressure',
+            'Increase in blood urea level',
+            'Loss of glucose through urine',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Kidney crystals are solid clusters of',
+        options: [
+            'Calcium nitrate and uric acid',
+            'Phosphate and uric acid',
+            'Calcium carbonate and uric acid',
+            'Calcium metabisulphite and uric acid',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'The glomerular filtrate consists of',
+        options: [
+            'Urea sodium chloride, fibrinogen and water',
+            'Glucose, amino acids, urea, oxytocin and calcitonin',
+            'Both and (2',
+            'Urea, glucose, salts and water',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            '"Columns of Bertini" in the kidney of mammals are formed as the extension of',
+        options: [
+            'Medulla into cortex ',
+            'Cortex into medulla',
+            'Medulla into pelvis ',
+            'Pelvis into ureter',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            "The liquid which is collected in the cavity of Bowman's capsule is",
+        options: [
+            'Concentrated urine',
+            'Blood plasma minus blood proteins',
+            'Glycogen and water',
+            'Sulphates and water',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Juxta glomerular apparatus is formed by cellular modification in the',
+        options: [
+            'Afferent arteriole and DCT',
+            'Efferent arteriole and PCT',
+            'Afferent arteriole and PCT',
+            'Efferent arteriole and DCT',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            "In which part of excretory system of mammals can you first use the term 'urine' for contained fluid ?",
+        options: [
+            "Bowman's capsule ",
+            'Loop of Henle',
+            'Collecting tubule ',
+            'Urinary bladder',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'ADH influences water permeability in the',
+        options: [
+            'Proximal tubule ',
+            'Distal tubule',
+            'Collecting tubule ',
+            'Both a and b',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'The plasma resembles its composition to the filtrate produced   by the glomerulus except the presence of',
+        options: ['Glucose ', 'Chloride', 'Amono acids ', 'Proteins'],
+        answer: 4,
+    },
+    {
+        question:
+            'When a person is suffering from poor renal reabsorption then which of the following will not help in the maintenance of blood volume',
+        options: [
+            'Decreased glomerular filtration',
+            'Increased ADH secretion',
+            'Decreased arterial pressure in kidney',
+            'Increased arterial pressure in kideny',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'In distal convoluted tubule of the nephrons.',
+        options: [
+            'Na reabsorption requires energy',
+            'Secretion of K ions does not require energy',
+            'Water reabsorption requires energy',
+            'Ammonia is secreted',
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            'Which of the following is totally reabsorbed in renal tubules ?',
+        options: ['Na ', 'K', 'H2O ', 'C6H12O6'],
+        answer: 4,
+    },
+    {
+        question: 'Part not belonging to uriniferous tubule is –',
+        options: [
+            'Glomerulus',
+            "Henle's loop",
+            'Distal convoluted tubule',
+            'Collecting duct',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Reabsorption of chloride ions from glomerular filtrate in uriniferous tubule occurs by –',
+        options: [
+            'Active transport ',
+            'Diffusion',
+            'Osmosis ',
+            'Brownian movement',
+        ],
+        answer: 2,
+    },
+    {
+        question:
+            'Which of the following set contains all groups of ureotelc animals?',
+        options: [
+            'Marine fishes, amphibians and mammals',
+            'Bony fishes, mammals and terrestrial insects',
+            'Aquatic insects, amphibians and land snails',
+            'Bony fishes, aquatic amphibians and terrestrial mammals',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'The projections of renal pelvis are called',
+        options: [
+            'Hiluses ',
+            'Calyces',
+            'Medullary pyramids ',
+            'Renal columns',
+        ],
+        answer: 2,
+    },
+    {
+        question: 'Main functions of kidney is –',
+        options: [
+            'Passive absorption ',
+            'Ultrafiltration',
+            'Selective reabsorption ',
+            'Both b and c',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'In man the two kidneys lie –',
+        options: [
+            'At the level of ovaries',
+            'At the same level',
+            'Left kidney at a higher level than the right one',
+            'Right kidney at a higher level than the left one',
+        ],
+        answer: 3 ,
+    },
+    {
+        question: 'Vasa rectae are peritubublar capillaries around –',
+        options: [
+            'Posterior part of alimentary canal',
+            'PCT',
+            'Loop of Henle',
+            'DCT',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Glomerular filtration rate would be decreased by –',
+        options: [
+            'Constriction of the efferent arteriole',
+            'An increase in afferent arteriolar pressure',
+            'Compression of the renal capsule',
+            'An increase in renal blood flow',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Duct of Bellini is concerned with –',
+        options: [
+            'Filtration of urine ',
+            'Purification of urine',
+            'Conduction of urine ',
+            'All the above',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            'Which of the following is directly responsible for increasing glomerular blood pressure and hence GFR?',
+        options: ['Aldosterone ', 'ANF', 'Angiotensin II ', 'Renin'],
+        answer: 3,
+    },
+    {
+        question: 'Urine contains how much of water –',
+        options: ['96% ', '90%', '70% ', 'None of these'],
+        answer: 1,
+    },
+    {
+        question:
+            'Which regulates reabsorption of salts from glomerular filtrate ?',
+        options: [
+            'Oxytocin ',
+            'Vassopressin',
+            'Glucoorticoides ',
+            'Mineral orticoids',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'A person who is on a long hunger strike and is surviving only on water, will have –',
+        options: [
+            'less amino acids in his urine',
+            'more glucose in his blood',
+            'less urea in his urine',
+            'more sodium in his urine',
+        ],
+        answer: 3,
+    },
+    {
+        question:
+            "Liquid part of the blood having undergone ultrafiltration from the glomerulus and reaching Bowman's capsule normally would not contain",
+        options: [
+            'Sugar (glucose) ',
+            'Sodium chloride',
+            'Creatinine ',
+            'Albumin',
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            'Find the incorrect statement regarding mechanism of urine formation in man',
+        options: [
+            'The glomerular filtration rate is about 125 ml per minute',
+            'The ultrafiltration is opposed by the colloidal osmotic pressure of plasma',
+            'The counter current systems contribute in diluting the urine',
+            'Aldosterone induces greater reabsorption of sodium',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Other function performed by kidney apart from excretion is',
+        options: [
+            'Osmoregulation ',
+            'Temperature regulation',
+            'Hormonal regualrtion ',
+            'Spermatogenesis',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Filtration pressure in human kidneys is about',
+        options: ['+ 15 mm Hg ', '+ 70 mm Hg', '+ 45 mm Hg ', '+55 mm Hg'],
+        answer: 1,
+    },
+    {
+        question: 'Presence of glucose in urine is called',
+        options: [
+            'Diabetes insipidus ',
+            'Diabetes mellitus',
+            'Galactosemia ',
+            'None of these',
+        ],
+        answer: 4,
+    },
+    {
+        question: 'Which of the following is correct ?',
+        options: [
+            'Water reabsorption in descending limb of loop and collecting duct are under similar conditions',
+            'Sodium reabsorption in ascending limb of loop and collecting duct occur under similar conditions.',
+            'Water reabsorption in descending limb of loop and collecting duct occur under different conditions.',
+            'None of these',
+        ],
+        answer: 1,
+    },
+    {
+        question: 'Diameter of the renal afferent vessel is',
+        options: [
+            'Same as that of efferent',
+            'Smaller than that of efferent',
+            'Larger than that of efferent',
+            'There is no efferent vessel',
+        ],
+        answer: 3,
+    },
+    {
+        question: 'Excretory products of mammalian embryo are eliminated by-',
+        options: ['Placenta ', 'Amniotic fluid', 'Allantois ', 'Ureters'],
+        answer: 1,
+    },
+    {
+        question:
+            'Effective filtration pressure in the glomerulus in kidney of man is about',
+        options: ['+ 75 mm Hg ', '+ 10 mm Hg', '+ 35 mm Hg ', '+ 50 mm Hg'],
+        answer: 2,
+    },
+    {
+        question:
+            'Person suffering from muscular dystrophy eliminate in urine a great amount of –',
+        options: ['Glucose ', 'Sulphate', 'Water ', 'Creatinine'],
+        answer: 4,
+    },
+    {
+        question: 'Healthy human does not excrete out in his urine –',
+        options: ['Uric acid ', 'Alanine', 'Creatinine ', 'B-complex vitamins'],
+        answer: 2,
+    },
+    {
+        question: 'Urea from the blood can be removed by',
+        options: ['Uraemia ', 'Diuresis', 'Dialysis ', 'Micturition'],
+        answer: 3,
+    },
+    {
+        question: 'Ureters act as urogenital ducts in',
+        options: [
+            'human males',
+            'human females',
+            'both female and male frog',
+            'frog male',
+        ],
+        answer: 4,
+    },
+];
